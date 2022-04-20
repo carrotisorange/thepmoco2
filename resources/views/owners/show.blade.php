6ab127d0590a77b45cd2b6f0c17ee9161c65c601
= <=>
2022-04-21 02:12:04 +0800

    made improvement in email.

--- resources/views/owners/show.blade.php
+++ resources/views/owners/show.blade.php
@@ -23,11 +23,11 @@
                 </div>
                 <h5 class="flex-1 text-right">
                     <x-button onclick="window.location.href='/owner/{{ $owner->uuid }}/edit'">
-                        Edit</x-button>
+                      <i class="fa-solid fa-pen-to-square"></i>&nbspEdit</x-button>
                     {{--<x-button
                         onclick="window.location.href='/unit/{{ $unit->uuid }}/contract/{{ Str::random(10) }}/create'">
                         Add Contract</x-button> --}}
-                    <x-button id="dropdownButton" data-dropdown-toggle="dropdown" type="button">Create <svg
+                    <x-button id="dropdownButton" data-dropdown-toggle="dropdown" type="button"><i class="fa-solid fa-circle-plus"></i>&nbspAdd<svg
                             class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7">
