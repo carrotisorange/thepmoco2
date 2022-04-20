25823a50018e156cd0f12fb3238591749f841fa7
= <=>
2022-04-21 02:18:58 +0800

    made improvement in email.

--- resources/views/owners/show.blade.php
+++ resources/views/owners/show.blade.php
@@ -93,7 +93,7 @@
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                                 <div class="mb-3">
-                                    <span class="text-center text-red">{{ Str::plural('Deed of sales',
+                                    <span class="text-center text-red">{{ Str::plural('Units',
                                         $deed_of_sales->count())}}
                                         ({{ $deed_of_sales->count() }})</span>
                                 </div>
