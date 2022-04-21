fd2bd1aa19da4f1b2ce69f662ff44662f54747c1
= <=>
2022-04-21 17:05:30 +0800

    improved the unit page.

--- resources/views/utilities/role-access-modal.blade.php
+++ resources/views/utilities/role-access-modal.blade.php
@@ -31,9 +31,8 @@
         <!-- Modal footer -->
         <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
             <p class="text-right">
-                <button data-modal-toggle="role-access-modal" type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                    accept</button>
+                <x-button data-modal-toggle="role-access-modal" type="button">
+                    Close</x-button>
             </p>
 
         </div>
