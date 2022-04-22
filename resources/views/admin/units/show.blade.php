699f4676945931863273b464988802f2c67cf2da
= <=>
2022-04-23 01:50:44 +0800

    added address columns to properties table.

--- resources/views/admin/units/show.blade.php
+++ resources/views/admin/units/show.blade.php
@@ -127,7 +127,7 @@
                                                     <div class="flex items-center">
                                                         <div class="flex-shrink-0 h-10 w-10">
                                                             <img class="h-10 w-10 rounded-full"
-                                                                src="/storage/{{ $tenant->photo_id }}">
+                                                                src="/storage/{{ $tenant->tenant->photo_id }}">
                                                         </div>
                                                         <div class="ml-4">
                                                             <div class="text-sm font-medium text-gray-900"><b>{{
