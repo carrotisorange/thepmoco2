5b18d7249097c02d3264fd29a38886f74ba44b2f
= <=>
2022-04-23 10:36:26 +0800

    added address columns to properties table.

--- resources/views/utilities/show-unit-results.blade.php
+++ resources/views/utilities/show-unit-results.blade.php
@@ -1,27 +1,16 @@
-<div class="grid grid-cols-6 gap-1 text-center">
+<div class="grid grid-cols-6 gap-1 md:grid:cols-1 sm:grid:cols-1">
     @foreach ($units as $unit)
-
     <div class="mt-5">
-        <x-button title="{{ $unit->unit }}" style="width: 85px; height: 60px;"
-            onclick="window.location.href='/unit/{{ $unit->uuid }}'" class="text-indigo-600">
-            <i class="fas fa-home fa-2x"></i>
-            
+        <x-button title="{{ $unit->unit }}" onclick="window.location.href='/unit/{{ $unit->uuid }}'">
+            <i class="fas fa-home fa-2x"></i> &nbsp
             <small> {{ $unit->unit }}</small>
         </x-button>
-       
-  
-    </div>
 
+        {{-- <x-button class="lg:hidden" title="{{ $unit->unit }}" onclick="window.location.href='/unit/{{ $unit->uuid }}'">
+            
+            <small> {{ $unit->unit }}</small>
+        </x-button> --}}
+
+    </div>
     @endforeach
 </div>
-
-
-{{-- @if($units->count())
-<span class="font-bold">Results ({{ $units->count() }})...</span>
-@else
-<p class="text-center text-red-600">No units found!</p>
-@endif
-@foreach($units as $unit)
-<a href="/unit/{{ $unit->uuid }}"><img src="/storage/{{ $unit->thumbnail }}"
-        class="p-2 bg-white border rounded max-w-md mt-5 mx-5 ml-5 mr-5 hover:bg-purple-600" alt="..." /></a>
-@endforeach --}}
