63a549ed7709bee285882c04f2a81dd2b15f1b1d
= <=>
2022-05-16 00:33:18 +0800

    added default to to due_date column in bills.

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Bill extends Model
 {
@@ -11,6 +12,7 @@ class Bill extends Model
 
     protected $attributes = [
         'status' => 'unpaid',
+        'due_date' => Carbon::now()->addDays(7)
     ];
 
     public function property()
