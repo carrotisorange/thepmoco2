05006e4d7d40cb14219b5bf75097fd7c126fd744
= <=>
2022-05-21 12:47:39 +0800

    added export button to ar.

--- resources/views/units/contracts/index.blade.php
+++ resources/views/units/contracts/index.blade.php
@@ -107,17 +107,16 @@
                                                 </x-td>
                                                 <x-td>{{ $item->interaction->interaction }}</x-td>
                                                 <x-td>
-                                                    <button id="dropdownDividerButton"
+                                                    <x-button id="dropdownDividerButton"
                                                         data-dropdown-toggle="dropdownDivider.{{ $item->uuid }}"
-                                                        class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
-                                                        type="button"><i
+                                                       type="button"><i
                                                             class="fa-solid fa-list-check"></i>&nbspOptions<svg
                                                             class="ml-2 w-4 h-4" fill="none" stroke="currentColor"
                                                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                 stroke-width="2" d="M19 9l-7 7-7-7">
                                                             </path>
-                                                        </svg></button>
+                                                        </svg></x-button>
 
                                                     <div id="dropdownDivider.{{ $item->uuid }}"
                                                         class="hidden z-10 w-44 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
