b243c18f8695090fcad928e7c285b6c23d3f43a5
= <=>
2022-06-07 15:05:01 +0800

    added privacy policy and terms of service upon registration.

--- app/Http/Livewire/ContractComponent.php
+++ app/Http/Livewire/ContractComponent.php
@@ -40,9 +40,8 @@ class ContractComponent extends Component
         $this->tenant = $tenant;
         $this->rent = $unit->rent;
         $this->discount = $unit->discount;
-  
+        $this->end = Carbon::now()->addYear()->format('Y-m-d');
         $this->start = Carbon::now()->format('Y-m-d');
-        $this->end = Carbon::parse($this->start)->addYear()->format('Y-m-d');
         $this->term = Carbon::now()->addYear()->diffInDays(Carbon::now());
         $this->sendContract = true;
       }
@@ -63,12 +62,6 @@ class ContractComponent extends Component
       {
         $this->validateOnly($propertyName);
       }
-
-      public function hydrateEnd()
-      {
-          $this->end = Carbon::parse($this->start)->addYear()->format('Y-m-d');
-      }
-
 
       public function submitForm()
       {
