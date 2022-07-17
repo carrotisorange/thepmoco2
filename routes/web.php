2cde790efb2576dcf2a387fce20f67e73bfda07c
= <=>
2022-07-18 00:42:11 +0800

    added thank you email and page.

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use App\Models\CheckoutOption;
 require __DIR__.'/auth.php';
 
 Route::get('/', function(){
-    return view('/properties');
+    return redirect('/properties');
 });
 
 //Routes for checkout pages
