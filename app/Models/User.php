5621781dc6902c87dbdb74488ebd595fd998a41e
= <=>
2022-07-28 17:10:45 +0800

    added feature to sales dashboard.

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function activities()
     {
-        return $this->hasMany(Activity::class);
+        return $this->hasMany(Activity::class)->orderBy('id', 'desc');
     }
 
     public function plan()
