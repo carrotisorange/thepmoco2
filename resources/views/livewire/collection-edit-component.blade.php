3ef7ed207e0ab1a0ad590310a50dfed81d61f74b
= <=>
2022-07-30 12:04:29 +0800

    improved sending mail.

--- resources/views/livewire/collection-edit-component.blade.php
+++ resources/views/livewire/collection-edit-component.blade.php
@@ -144,26 +144,18 @@
 
         </div>
 
-{{-- 
+
         @if($tenant->email)
         <div class="mt-5 p-2 bg-white border-b border-gray-200">
             <div class="flex flex-wrap mb-6">
                 <div class="mt-2 w-full md:w-full  mb-6 md:mb-0">
                     <div>
-                        <div class="form-check">
-                            <input wire:model="sendPayment" form="edit-form"
-                                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                                type="checkbox" value="{{ old('sendPayment'), $sendPayment }}"
-                                name="sendPayment">
-                            <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
-                                Send payment details to {{ $tenant->email }}
-                            </label>
-                        </div>
+                       Acknowledgement Receipt will be sent to {{ $tenant->email }}
                     </div>
                 </div>
             </div>
         </div>
-        @endif --}}
+        @endif
 
         <div class="mt-5 p-2">
             <p class="text-right">
