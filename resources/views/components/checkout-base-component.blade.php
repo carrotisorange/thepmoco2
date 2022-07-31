23107e22aa3392a27d788bff98eecd322c7faa10
= <=>
2022-07-31 15:53:17 +0800

    improved checkout page.

--- resources/views/components/checkout-base-component.blade.php
+++ resources/views/components/checkout-base-component.blade.php
@@ -24,6 +24,41 @@
 
                     </div>
 
+                    <!-- Settings Dropdown -->
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                   
+                                    <div class="ml-2">The Property Manager</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link href="/">
+                                   Login
+                                </x-dropdown-link>
+                                <x-dropdown-link href="/select-a-plan">
+                                    Plans
+                                </x-dropdown-link>
+                      
+
+
+                            </x-slot>
+                        </x-dropdown>
+
+                    </div>
+
                     <!-- Hamburger -->
                     <div class="-mr-2 flex items-center sm:hidden">
                         <button @click="open = ! open"
@@ -39,6 +74,27 @@
                         </button>
                     </div>
                 </div>
+            </div>
+            <!-- Responsive Navigation Menu -->
+            <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+
+                <!-- Responsive Settings Options -->
+                <div class="pt-4 pb-1 border-t border-gray-200">
+
+                    <div class="pt-2 pb-3 space-y-1">
+                        <x-responsive-nav-link href="/">
+                            {{ __('Login') }}
+                        </x-responsive-nav-link>
+                    </div>
+                    <div class="pt-2 pb-3 space-y-1">
+                        <x-responsive-nav-link href="/select-a-plan">
+                            {{ __('Plans') }}
+                        </x-responsive-nav-link>
+                    </div>
+
+
+                </div>
+
             </div>
 
             <main>
