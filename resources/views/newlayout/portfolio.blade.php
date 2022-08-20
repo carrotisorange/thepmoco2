fe48c0cb1a0ec443e658b2598bbfa4aa2461f5a9
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-08-20 11:41:38 +0800

    created portfolio page

--- resources/views/newlayout/portfolio.blade.php
+++ resources/views/newlayout/portfolio.blade.php
@@ -222,171 +222,103 @@
     <main class="flex-1 pb-8 h-screen y-screen overflow-y-scroll">
     <div class="mt-8">
         <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          <!-- card -->
-          <div class="bg-white overflow-hidden shadow rounded-lg">
-              <div class="p-5">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0">
-                    
-                    
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                  <div class="ml-5 w-0 flex-1">
-                    <dl>
-                      <dt class="text-xl font-semibold font-body text-gray-500 truncate">Welcome back, Basilio</dt>
-                      <dd>
-                        
-                        <img class="h-45 w-auto" src=brands/juan.png>
-                      </dd>
-                    </dl>
-                  </div>
-                </div>
-              </div>
-              <div class="px-5 py-3">
-                <div class="text-sm">
-                  <p class="font-medium text-center"> Update your profile. </p>
-                  </div>
-                  
-              </div>
-              <div class="grid grid-cols-3 gap-4 place-content-center ">
-              <img class=" ml-8 h-10 w-auto flex " src=brands/contract.png>
-              <img class=" ml-8 h-10 w-auto flex " src=brands/contract.png>
-              <img class=" ml-8 h-10 w-auto flex " src=brands/contract.png>
-              <button class="type= py-2.5 px-5 mr-2 mb-2 text-sm font-light text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Contracts</button> 
-              <button class="type= py-2.5 px-5 mr-2 mb-2 text-sm font-light text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Unpaid Bills</button> 
-              <button class="type= py-2.5 px-5 mr-2 mb-2 text-sm font-light text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Concerns</button>  
-            </div>
-            
-            </div>
+    <!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<div class="bg-white">
+  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Welcome back, Juan</h2>
 
-
-            <!-- card -->
-            <div class="bg-white overflow-hidden">
-              <div class="p-5">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0">
-                    <!-- Heroicon name: outline/scale -->
-                    <img class="h-10 w-auto" src=brands/megaphone.png>
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                  <div class="ml-5 w-0 flex-1">
-                    
-                    <dl>
-                      <dt class="text-3xl font-medium text-black-500 truncate">Announcements:</dt>
-                      
-                      <dd>
-                      </dd>
-                    </dl>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-indigo-50 px-5 py-8 mb-5 rounded-lg">
-                <div class="text-sm">
-                <a href="#" class="font-medium text-cyan-700 hover:text-cyan-900"> Your contract will expire in 2 days. </a>
-                <div button type="button" class="items-center px-2.5 py-1.5 border w-20 mt-5 border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Renew</button></div>
-                </div>
-              </div>
-              <div class="bg-indigo-50 px-5 py-8 mb-5 rounded-lg">
-                <div class="text-sm">
-                  <a href="#" class="font-medium text-cyan-700 hover:text-cyan-900"> You have unpaid bills for unit #2. </a>
-                  <div button type="button" class="items-center px-2.5 py-1.5 border w-20 mt-5 border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pay now</button></div>
-                </div>
-              </div>
-              <div class="bg-indigo-50 px-5 py-8 mb-5 rounded-lg">
-                <div class="text-sm">
-                  <a href="#" class="font-medium text-cyan-700 hover:text-cyan-900"> You have unpaid bills. </a>
-                  <div button type="button" class="items-center px-2.5 py-1.5 border w-20 mt-5 border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pay now</button></div>
-                </div>
-              </div>
-            </div>
-            <!-- card -->
-            <div class="bg-white">
-              <div class="p-5">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0">
-                    <!-- Heroicon name: outline/scale -->
-                    
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                  <div class="ml-0 w-0 flex-1">
-                    <dl>
-                      <dt class="text-sm font-medium text-gray-500 truncate">Friday, August 19, 2022</dt>
-                      <dd>
-                        <div class="text-lg font-medium text-gray-900">$30,659.45</div>
-                        <h2 class="text-lg leading-3 ml-0 font-medium text-gray-600 mt-10">Today</h2>
-                      </dd>
-                    </dl>
-                  </div>
-                </div>
-              </div>
-              
-              <div class="bg-gray-50 px-5 py-3">
-                <div class="text-sm">
-                <div class="mb-5 max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
-      <div class="p-4">
-        <div class="flex items-start">
-          <div class="flex-shrink-0">
-            <!-- Heroicon name: outline/check-circle -->
-            <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </div>
-          <div class="ml-3 w-0 flex-1 pt-0.5">
-            <p class="text-sm font-medium text-gray-900">Successfully paid!</p>
-            <p class="mt-1 text-sm text-gray-500">You paid 16,000.00 for rent.</p>
-          </div>
-                </div>
-              </div>
-</div>
-<div class="mb-5 max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
-              <div class="p-4">
-        <div class="flex items-start">
-          <div class="flex-shrink-0">
-            <!-- Heroicon name: outline/check-circle -->
-            <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </div>
-          <div class="ml-3 w-0 flex-1 pt-0.5">
-            <p class="text-sm font-medium text-gray-900">Successfully paid!</p>
-            <p class="mt-1 text-sm text-gray-500">You paid 16,000.00 for rent.</p>
-          </div>
-          </div>
-</div>
-</div>
-<div class="mb-5 max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
-      <div class="p-4">
-        <div class="flex items-start">
-          <div class="flex-shrink-0">
-            <!-- Heroicon name: outline/check-circle -->
-            <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </div>
-          <div class="ml-3 w-0 flex-1 pt-0.5">
-            <p class="text-sm font-medium text-gray-900">Successfully paid!</p>
-            <p class="mt-1 text-sm text-gray-500">You paid 16,000.00 for rent.</p>
-          </div>
-                </div>
-              </div>
-</div>
-</div>
-</div>
-
-<div button type="button" class=" justify-self-end items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm 
-text-white text-center bg-gray-600 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">View More</button>
-</div>
-
-
+    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+      <div class="group relative">
+        <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
         </div>
-
-
+        <div class="mt-4 flex justify-between">
+          <div>
+            <h3 class="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Basic Tee
+              </a>
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">Black</p>
+          </div>
+          <p class="text-sm font-medium text-gray-900">$35</p>
         </div>
-      </aside>
+      </div>
+
+      <!-- More products... -->
+    </div>
+  </div>
+</div>
+        
+       <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="px-4 sm:px-6 lg:px-8">
+  <div class="sm:flex sm:items-center">
+    <div class="sm:flex-auto">
+      <h1 class="text-xl font-semibold text-gray-900">Properties</h1>
+      <p class="mt-2 text-sm text-gray-700">Select a property.</p>
+    </div>
+    <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+      <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Export</button>
+    </div>
+  </div>
+  <div class="mt-8 flex flex-col">
+    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Transaction ID</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Company</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Share</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Commision</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Price</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Quantity</th>
+                <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Net amount</th>
+                <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+              <tr>
+                <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-6">AAPS0L</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">Chase &amp; Co.</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">CAC</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">+$4.37</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">$3,509.00</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">12.00</td>
+                <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">$4,397.00</td>
+                <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, AAPS0L</span></a>
+                </td>
+              </tr>
+
+              <!-- More properties... -->
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+   
       <!-- Footer -->
 <footer class="bg-white">
   <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
