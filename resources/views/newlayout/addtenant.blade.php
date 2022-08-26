1734f7d1d0cb7f131dd82a80cb49c2bcac077e86
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-08-27 02:19:50 +0800

    created add a tenant page

--- resources/views/newlayout/addtenant.blade.php
+++ resources/views/newlayout/addtenant.blade.php
@@ -266,7 +266,7 @@
 
           <!-- Main area -->
           <main class="flex-1 pb-8 h-screen y-screen overflow-y-scroll">
-            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="mx-auto px-4 sm:px-6 lg:px-8">
 
 <!--
   This example requires Tailwind CSS v2.0+ 
@@ -301,18 +301,22 @@
 <div class="mt-10 lg:border-t lg:border-b lg:border-gray-200">
   <nav class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Progress">
     <ol role="list" class="rounded-md overflow-hidden lg:flex lg:border-l lg:border-r lg:border-gray-200 lg:rounded-none">
-      <li class="relative overflow-hidden lg:flex-1">
-        <div class="border border-gray-200 overflow-hidden border-b-0 rounded-t-md lg:border-0">
-          <!-- Completed Step -->
+
+
+    <li class="relative overflow-hidden lg:flex-1">
+        <div class="border border-gray-200 overflow-hidden lg:border-0">
+          <!-- Current Step -->
           <a href="#" aria-current="step">
             <span class="absolute top-0 left-0 w-1 h-full lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
             <span class="px-6 py-5 flex items-start text-sm font-medium lg:pl-9">
               <span class="flex-shrink-0">
-                <span class="w-10 h-10 flex items-center justify-center border-2 border-indigo-600 rounded-full">
+                <span class="w-10 h-10 flex items-center justify-center border-2  rounded-full">
                   <span class="text-indigo-600">01</span>
                 </span>
+              </span>
               <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                <span class="text-sm font-medium">Tenant Information</span>
+                <span class="text-sm font-medium text-indigo-600">Tenant Information Sheet</span>
+                
               </span>
             </span>
           </a>
@@ -323,7 +327,7 @@
         <div class="border border-gray-200 overflow-hidden lg:border-0">
           <!-- Current Step -->
           <a href="#" aria-current="step">
-            <span class="absolute top-0 left-0 w-1 h-fulllg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
+            <span class="absolute top-0 left-0 w-1 h-full lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
             <span class="px-6 py-5 flex items-start text-sm font-medium lg:pl-9">
               <span class="flex-shrink-0">
                 <span class="w-10 h-10 flex items-center justify-center border-2  rounded-full">
@@ -331,11 +335,12 @@
                 </span>
               </span>
               <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                <span class="text-sm font-medium text-indigo-600">Application form</span>
-                <span class="text-sm font-medium text-gray-500">Cursus semper viverra.</span>
+                <span class="text-sm font-medium text-indigo-600">Guardian Information</span>
+                
               </span>
             </span>
           </a>
+
 
           <!-- Separator -->
           <div class="hidden absolute top-0 left-0 w-3 inset-0 lg:block" aria-hidden="true">
@@ -347,19 +352,19 @@
       </li>
 
       <li class="relative overflow-hidden lg:flex-1">
-        <div class="border border-gray-200 overflow-hidden border-t-0 rounded-b-md lg:border-0">
-          <!-- Upcoming Step -->
-          <a href="#" class="group">
-            <span class="absolute top-0 left-0 w-1 h-full bg-transparent group-hover:bg-gray-200 lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
+        <div class="border border-gray-200 overflow-hidden lg:border-0">
+          <!-- Current Step -->
+          <a href="#" aria-current="step">
+            <span class="absolute top-0 left-0 w-1 h-full lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
             <span class="px-6 py-5 flex items-start text-sm font-medium lg:pl-9">
               <span class="flex-shrink-0">
-                <span class="w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full">
-                  <span class="text-gray-500">03</span>
+                <span class="w-10 h-10 flex items-center justify-center border-2  rounded-full">
+                  <span class="text-indigo-600">03</span>
                 </span>
               </span>
               <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                <span class="text-sm font-medium text-gray-500">Preview</span>
-                <span class="text-sm font-medium text-gray-500">Penatibus eu quis ante.</span>
+                <span class="text-sm font-medium text-indigo-600">Reference</span>
+                
               </span>
             </span>
           </a>
@@ -370,24 +375,32 @@
               <path d="M0.5 0V31L10.5 41L0.5 51V82" stroke="currentcolor" vector-effect="non-scaling-stroke" />
             </svg>
           </div>
+</li>
 
-          <li class="relative overflow-hidden lg:flex-1">
-        <div class="border border-gray-200 overflow-hidden border-b-0 rounded-t-md lg:border-0">
-          <!-- Completed Step -->
+           <li class="relative overflow-hidden lg:flex-1">
+        <div class="border border-gray-200 overflow-hidden lg:border-0">
+          <!-- Current Step -->
           <a href="#" aria-current="step">
-            <span class="absolute top-0 left-0 w-1 h-full  lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
+            <span class="absolute top-0 left-0 w-1 h-full lg:w-full lg:h-1 lg:bottom-0 lg:top-auto" aria-hidden="true"></span>
             <span class="px-6 py-5 flex items-start text-sm font-medium lg:pl-9">
               <span class="flex-shrink-0">
                 <span class="w-10 h-10 flex items-center justify-center border-2  rounded-full">
-                  <span class="text-indigo-600">04</span>
+                  <span class="text-indigo-600">02</span>
                 </span>
+              </span>
               <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                <span class="text-sm font-medium">Tenant Information</span>
-                <span class="text-sm font-medium text-gray-500">Vitae sed mi luctus laoreet.</span>
+                <span class="text-sm font-medium text-indigo-600">Contract</span>
+                
               </span>
             </span>
           </a>
         </div>
+
+        <div class="hidden absolute top-0 left-0 w-3 inset-0 lg:block" aria-hidden="true">
+            <svg class="h-full w-full text-gray-300" viewBox="0 0 12 82" fill="none" preserveAspectRatio="none">
+              <path d="M0.5 0V31L10.5 41L0.5 51V82" stroke="currentcolor" vector-effect="non-scaling-stroke" />
+            </svg>
+          </div>
       </li>
 
       <li class="relative overflow-hidden lg:flex-1">
@@ -402,16 +415,107 @@
                 </span>
               </span>
               <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                <span class="text-sm font-medium text-indigo-600">Application form</span>
-                <span class="text-sm font-medium text-gray-500">Cursus semper viverra.</span>
+                <span class="text-sm font-medium text-indigo-600">Bill Information</span>
               </span>
             </span>
           </a>
         </div>
+
+        <div class="hidden absolute top-0 left-0 w-3 inset-0 lg:block" aria-hidden="true">
+            <svg class="h-full w-full text-gray-300" viewBox="0 0 12 82" fill="none" preserveAspectRatio="none">
+              <path d="M0.5 0V31L10.5 41L0.5 51V82" stroke="currentcolor" vector-effect="non-scaling-stroke" />
+            </svg>
+          </div>
       </li>
     </ol>
   </nav>
+
+
 </div>
+
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<form class="space-y-6" action="#" method="POST">
+ 
+
+
+        
+
+        
+
+  <div class="mt-5 px-4 py-5 sm:rounded-lg sm:p-6">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="grid grid-cols-6 gap-6">
+          <div class="col-span-6 sm:col-span-3">
+            <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
+            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-3">
+            <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
+            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-4">
+            <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
+            <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-3">
+            <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
+            <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+              <option>United States</option>
+              <option>Canada</option>
+              <option>Mexico</option>
+            </select>
+          </div>
+
+          <div class="col-span-6">
+            <label for="street-address" class="block text-sm font-medium text-gray-700">Street address</label>
+            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+            <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+            <input type="text" name="city" id="city" autocomplete="address-level2" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+            <label for="region" class="block text-sm font-medium text-gray-700">State / Province</label>
+            <input type="text" name="region" id="region" autocomplete="address-level1" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+            <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP / Postal code</label>
+            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="flex justify-end">
+    <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
+    <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+  </div>
+</form>
 
     
 </div>
