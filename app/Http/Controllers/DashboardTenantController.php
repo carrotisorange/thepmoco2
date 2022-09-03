83c7758e077ec6eec52bbb073f35414c011f3c9c
= <=>
2022-09-03 13:01:58 +0800

    added option to moveout in tenant portal.

--- app/Http/Controllers/DashboardTenantController.php
+++ app/Http/Controllers/DashboardTenantController.php
@@ -17,21 +17,21 @@ class DashboardTenantController extends Controller
     public function show_contracts($role_id, User $user)
     {        
         return view('portal.tenants.contracts',[
-            'contracts' => Tenant::findOrFail($user->tenant_uuid)->contracts()->paginate(5)
+            'contracts' => Tenant::findOrFail($user->tenant_uuid)->contracts()->orderBy('start', 'desc')->paginate(5)
         ]);
     }
 
     public function show_bills($role_id, User $user)
     {
         return view('portal.tenants.bills',[
-            'bills' => Tenant::findOrFail($user->tenant_uuid)->bills()->orderBy('created_at', 'desc')->paginate(5)
+            'bills' => Tenant::findOrFail($user->tenant_uuid)->bills()->orderBy('bill_no', 'desc')->paginate(5)
         ]);
     }
 
     public function show_collections($role_id, User $user)
     {
         return view('portal.tenants.collections',[
-            'collections' => Tenant::findOrFail($user->tenant_uuid)->acknowledgementreceipts()->paginate(5)
+            'collections' => Tenant::findOrFail($user->tenant_uuid)->acknowledgementreceipts()->orderBy('ar_no','desc')->paginate(5)
         ]);
     }
 
