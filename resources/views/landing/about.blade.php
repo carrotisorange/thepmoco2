778fcb389cd82c029aff46451db18cbca0cf73b5
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-09-07 23:42:05 +0800

    updated about page

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -262,7 +262,7 @@
           </div>
           <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
             <h1 class="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
-              <span class="block text-white">About the PMO</span>
+              <span class="block text-gray-800">About the PMO</span>
               
             </h1>
             <p class="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">The service you deserve, with the people you trust.</p>
@@ -305,18 +305,13 @@
 <!-- Blog section -->
 <div class="relative bg-gray-white py-16 sm:py-24 lg:py-32">
         <div class="relative">
-          <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Story</p>
-            <p class="mx-auto mt-5  text-xl text-white">Let us take care of your business process, so you can focus on growing your business!
-
-</p>
-          </div>
+        <img class="h-72 w-full object-cover" src="{{ asset('/brands/build.jpg') }}" alt="pmo logo">
 
           <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
           <!-- Content area -->
-          <div class="pt-12 sm:pt-16 lg:pt-20">
+          <div class="pt-8 sm:pt-8 lg:pt-8">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">How our Journey Started</h2>
-            <div class="mt-6 space-y-6 text-gray-500">
+            <div class="mt-10 space-y-6 text-gray-500">
               <p class="text-lg">When we first started out as property managers, we followed the old-school methods. During leasing procedures, we used the traditional way of paper and pen to sign up tenant info sheets, contracts, billing statements, and receipts. It took an entire day to process a single report! 
 </p>
               <p class="text-base leading-7">At one point, our operations are so wrapped up in administrative work that we are spending less time strengthening our customer relations. We spend so much time looking for documents and less time on satisfying customer requests.</p>
@@ -335,171 +330,91 @@
 
        
       </div>
-     
 
-    <!-- CTA Section -->
-   <!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<div>
-  <div class="">
-    <div class="relative bg-white shadow-xl">
-      <h2 class="sr-only">Contact us</h2>
-
-      <div class="grid grid-cols-1 lg:grid-cols-3">
-        <!-- Contact information -->
-        <div class="px-5 relative overflow-hidden bg-gray-700 py-10 sm:px-10 xl:p-12">
-          <div class="pointer-events-none absolute inset-0 sm:hidden" aria-hidden="true">
-            <svg class="absolute inset-0 h-full w-full" width="343" height="388" viewBox="0 0 343 388" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
-              <path d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z" fill="url(#linear1)" fill-opacity=".1" />
-              <defs>
-                <linearGradient id="linear1" x1="254.553" y1="107.554" x2="961.66" y2="814.66" gradientUnits="userSpaceOnUse">
-                  <stop stop-color="#fff"></stop>
-                  <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
-                </linearGradient>
-              </defs>
-            </svg>
+      <!-- Testimonial section -->
+      <div class="bg-gradient-to-r from-purple-500 to-gray-600 pb-16 lg:relative lg:z-10 lg:pb-0">
+        <div class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
+          <div class="relative lg:-my-8">
+            <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"></div>
+           
           </div>
-          <div class="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 sm:block lg:hidden" aria-hidden="true">
-            <svg class="absolute inset-0 h-full w-full" width="359" height="339" viewBox="0 0 359 339" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
-              <path d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z" fill="url(#linear2)" fill-opacity=".1" />
-              <defs>
-                <linearGradient id="linear2" x1="192.553" y1="28.553" x2="899.66" y2="735.66" gradientUnits="userSpaceOnUse">
-                  <stop stop-color="#fff"></stop>
-                  <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
-          <div class="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 lg:block" aria-hidden="true">
-            <svg class="absolute inset-0 h-full w-full" width="160" height="678" viewBox="0 0 160 678" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
-              <path d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z" fill="url(#linear3)" fill-opacity=".1" />
-              <defs>
-                <linearGradient id="linear3" x1="192.553" y1="325.553" x2="899.66" y2="1032.66" gradientUnits="userSpaceOnUse">
-                  <stop stop-color="#fff"></stop>
-                  <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
-          <h3 class="text-3xl font-medium text-white">Contact Us</h3>
-          <h3 class="mt-2 text-xl font-medium text-white">The PMO Co.</h3>
-          <p class="mt-6 max-w-3xl text-base text-indigo-50">Asian Institute of Management - Dado Banatao Incubator Benavidez Street, corner Trasierra, Legazpi Village, Makati, 1229 Metro Manila</p>
-          <dl class="mt-8 space-y-6">
-            <dt><span class="sr-only">Phone number</span></dt>
-            <dd class="flex text-base text-indigo-50">
-              <!-- Heroicon name: outline/phone -->
-              <svg class="h-6 w-6 flex-shrink-0 text-indigo-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-              </svg>
-              <span class="ml-3">(+63) 916 779 9750</span>
-            </dd>
-            <dt><span class="sr-only">Email</span></dt>
-            <dd class="flex text-base text-indigo-50">
-              <!-- Heroicon name: outline/envelope -->
-              <svg class="h-6 w- flex-shrink-0 text-indigo-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-              </svg>
-              <span class="ml-3">support@workcation.com</span>
-            </dd>
-          </dl>
-          <ul role="list" class="mt-8 flex space-x-12">
-            <li>
-              <a class="text-indigo-200 hover:text-indigo-100" href="#">
-                <span class="sr-only">Facebook</span>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" aria-hidden="true">
-                  <path d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1" fill="currentColor" />
-                </svg>
-              </a>
-            </li>
-            <li>
-            
-            </li>
-          </ul>
-        </div>
-
-        <!-- Contact form -->
-        <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-          
-          <form action="#" method="POST" class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-            <div>
-              <label for="first-name" class="block text-sm font-medium text-gray-900">First name</label>
-              <div class="mt-1">
-                <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="bg-purple-100 block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-              </div>
+          <div class="mt-12 lg:col-span-1 lg:m-0 lg:pl-8">
+            <div class="mx-auto px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20">
+              <blockquote>
+                <div>
+                  
+                  <p class="mt-6 text-2xl font-medium text-white">We carry the values of hard work, integrity, and outstanding client service into making property management simpler!</p>
+                </div>
+                <footer class="mt-6">
+                  
+                  
+                </footer>
+              </blockquote>
             </div>
-            <div>
-              <label for="last-name" class="block text-sm font-medium text-gray-900">Last name</label>
-              <div class="mt-1">
-                <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="bg-purple-100 block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-              </div>
-            </div>
-            <div>
-              <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
-              <div class="mt-1">
-                <input id="email" name="email" type="email" autocomplete="email" class="bg-purple-100 block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-              </div>
-            </div>
-            <div>
-              
-            <div class="flex justify-between">
-                <label for="message" class="block text-sm font-medium text-gray-900">Message</label>
-                <span id="message-max" class="text-sm text-gray-500">Max. 500 characters</span>
-              </div>
-              <div class="mt-1">
-                <textarea id="message" name="message" rows="1" class="bg-purple-100 block w-full rounded-md border-gray-300 py-3 px-4 text-gray-900 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" aria-describedby="message-max"></textarea>
-              </div>
-            </div>
-
-          
-
-            <div class="sm:col-span-2">
-             
-            <label for="country" class="block text-sm font-medium text-gray-700"> Property Type: </label>
-          <div class="mt-5">
-            
-              <option>Condominium Association</option>
-              <option>Condominium Units</option>
-              <option>Student Accomodation</option>
-              <option>HOA</option>
-              <option>Dormitory</option>
-              <option>Commercial</option>
-              <option>Self storage</option>
-              <option>Senior Living</option>
-              <option>Residential Apartments</option>
-              <option>Bed and breakfast</option>
-              <option>Transient</option>
-              
-            </select>
           </div>
         </div>
+      </div>
 
-            
+      <!-- Stats section -->
+    <div class="relative bg-gray-900">
+      <div class="absolute inset-x-0 bottom-0 h-80 xl:top-0 xl:h-full">
+        <div class="h-full w-full xl:grid xl:grid-cols-2">
+          <div class="h-full xl:relative xl:col-start-2">
+            <img class="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0" src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100" alt="People working on laptops">
+            <div aria-hidden="true" class="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"></div>
+          </div>
+        </div>
+      </div>
+      <div class="mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-flow-col-dense xl:grid-cols-2 xl:gap-x-8">
+        <div class="relative pt-12  xl:col-start-1 xl:pb-24">
+          
+          <p class="mt-3 text-3xl font-bold tracking-tight text-white">The PMO is HIPER-Focused</p>
+          <p class="mt-5 text-lg text-gray-300">Our search is focused on our client’s requirements. We respect their time, finances, or any other boundaries and offer them world-class services</p>
+          <div class="mt-10 grid grid-cols-1 gap-y-5 gap-x-6 sm:grid-cols-5">
+            <p>
+              <span class="block text-2xl font-bold text-white">H</span>
+              <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">High</span>-tech Performance</span>
+            </p>
 
+            <p>
+              <span class="block text-2xl font-bold text-white">I</span>
+              <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Integrity</span> </span>
+            </p>
 
-            <div class="sm:col-span-2 sm:flex sm:justify-end">
-              <button type="submit" class="mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-purple-500 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Send</button>
-            </div>
-          </form>
+            <p>
+              <span class="block text-2xl font-bold text-white">P</span>
+              <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Passion</span></span>
+            </p>
+
+            <p>
+              <span class="block text-2xl font-bold text-white">E</span>
+              <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Excellent</span> communication</span>
+            </p>
+
+            <p>
+              <span class="block text-2xl font-bold text-white">R</span>
+              <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Respect</span></span>
+            </p>
+
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+     
+    <div class="bg-white">
+      <div class="mx-auto max-w-4xl py-16 px-4 sm:px-6 sm:py-24 lg:flex lg:max-w-7xl lg:items-center lg:justify-between lg:px-8">
+        <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <span class="block">Register and start your journey with us now!</span>
+          <span class="-mb-1 block bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text pb-1 text-transparent">Get in touch or create an account.</span>
+        </h2>
+        <div class="mt-6 space-y-4 sm:flex sm:space-y-0 sm:space-x-5">
+          <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-3 text-base font-medium text-white shadow-sm hover:from-purple-700 hover:to-indigo-700">Start Free Trial</a>
+          
+        </div>
+      </div>
+    </div>
 
+   
   </main>
 
   <footer class="bg-gray-50" aria-labelledby="footer-heading">
