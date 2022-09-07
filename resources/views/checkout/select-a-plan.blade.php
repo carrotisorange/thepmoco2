b37ccd7f604ded076c69ba231e5e2fe595157493
= <=>
2022-09-07 22:33:10 +0800

    added the privacy policy and terms of service on the sign up page.

--- resources/views/checkout/select-a-plan.blade.php
+++ resources/views/checkout/select-a-plan.blade.php
@@ -95,9 +95,9 @@
 
 
                             <div class="text-sm text-center">
-                                By clicking the sign up below, you agree to the <a href="terms"
+                                By clicking the sign up below, you agree to the <a target="_blank" href="https://www.thepropertymanager.online/termsandconditions"
                                     class="font-medium text-indigo-600 hover:text-indigo-500">Terms & Conditions</a> and
-                                <a href="privacy" class="font-medium text-indigo-600 hover:text-indigo-500">Privacy
+                                <a target="_blank" href="https://www.thepropertymanager.online/privacypolicy" class="font-medium text-indigo-600 hover:text-indigo-500">Privacy
                                     Policy</a>
                             </div>
 
