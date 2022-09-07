86e16bb709ca31399f60660548f1fa29b630b7bc
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-09-07 19:34:58 +0800

    search filter

--- resources/views/newlayout/employees.blade.php
+++ resources/views/newlayout/employees.blade.php
@@ -325,12 +325,20 @@
         <input type="search" id="default-search" class="bg-white block p-4 pl-10 w-full text-sm h-5 text-gray-90 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required>
         
     </div>
+
+    <div class="mt-2">
+      <p class="text-sm text-center text-gray-500">
+        Showing
+        <span class="font-medium">1</span>
+        result
+      </p>
+    </div>
+    
 </div>
 
 </form>
 
-<div class="sm:col-span-">
-<form>   
+
     
     <div class="relative w-full">
         
@@ -359,11 +367,6 @@
           </div>
         
       
-</div
-
-
-    
-</form>
 
 
 
@@ -373,7 +376,7 @@
 </div>
 
 
-    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="-my-8 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
 
     
 
