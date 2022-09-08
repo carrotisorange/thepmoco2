4dbbe85a0dee41f8769fa549606794cf0e712fcc
= <=>
2022-09-08 16:19:35 +0800

    added label when error occurs on login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                             <div>
                                 <label for="username" class="block text-sm font-medium text-gray-700"> Username </label>
                                 <div class="mt-1">
-                                    <input id="username" name="username" type="username" autocomplete="username"
+                                    <input id="username" name="username" type="username" autocomplete="username" value="{{ old('username') }}"
                                         required
                                         class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                 </div>
