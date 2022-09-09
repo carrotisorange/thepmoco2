faa21798b7b843d85dba47d135a71987576c1703
= <=>
2022-09-09 21:05:43 +0800

    improved the verificaiton email page.

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,100 +1,81 @@
-<x-checkout-base-component>
+<x-new-layout-base>
     @section('title', 'Verify Email | The Property Manager')
 
-    <div class="bg-gray-50 sm:pt-16">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="max-w-4xl mx-auto text-center">
-                <h2 class="text-xs font-bold text-gray-900 sm:text-4xl">
 
-                </h2>
-                <p class="text-xl text-gray-500 sm:mt-4">
-                    Thanks for signing up! Before getting started, could you verify your email address by
-                    clicking on
-                    the
-                    link we just emailed to you? If you didn't receive the email, we will gladly send you
-                    another.
 
-                    @if (session('status') == 'verification-link-sent')
-                    <div class="font-medium text-sm text-white-600">
-                        A new verification link has been sent to the email address you provided during
-                        registration.
-                    </div>
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <!--
+      This example requires Tailwind CSS v2.0+ 
+      
+      This example requires some changes to your config:
+      
+      ```
+      // tailwind.config.js
+      module.exports = {
+        // ...
+        plugins: [
+          // ...
+          require('@tailwindcss/aspect-ratio'),
+        ],
+      }
+      ```
+    -->
 
-                @endif
+        <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 mt-5">
+            <div class=" flex items-center justify-center">
+                <img class="h-56 w-auto" src="{{ asset('/brands/thanks.png') }}">
+            </div>
+            <h2 class="pt-3 pb-5 text-center text-2xl font-bold tracking-tight text-gray-900 font-pop">Thanks
+                for signing up!</h2>
+            <p class="text-center text-md font-medium text-medium text-gray-700">Your journey with The Property
+                Manager Online starts when you</p>
+            <p class="text-center text-md font-medium text-medium text-gray-700">verify your email by clicking
+                the link we sent you.</p>
+            </p>
 
-                <div class="mt-4 flex items-center justify-between p-2">
-                    <form method="POST" action="{{ route('verification.send') }}">
-                        @csrf
 
-                        <div>
-                            <x-button>
-                                {{ __('Resend Verification Email') }}
-                            </x-button>
-                        </div>
-                    </form>
 
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
+            <p class="mt-5 text-center text-md font-normal text-gray-500">If you did not receive an email, click
+                on the resend button below.
 
-                        <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                            {{ __('Log Out') }}
+            </p>
+
+
+            <div class="mt-5 flex items-center justify-center">
+                <form method="POST" action="{{ route('verification.send') }}">
+                    @csrf
+
+                    <div>
+                        <button type="submit"
+                            class="mr-3 w-56 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white text-center bg-purple-900 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Resend verification email
                         </button>
-                    </form>
-                </div>
-                </p>
-            </div>
-        </div>
-        <div class="bg-white sm:pb-16">
-            <div class="relative">
-                <div class="absolute inset-0 h-1/2 bg-gray-50"></div>
-                <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    </div>
+                </form>
 
-                </div>
+
+
+
             </div>
+
+
         </div>
+{{-- 
+        <div class="mt-3 flex items-center justify-center">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <button type="submit"
+                    class="w-32 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white text-center bg-gray-900 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Log Out</button>
+            </form>
+
+        </div> --}}
+        @if (session('status') == 'verification-link-sent')
+        <p class="mt-5 text-center text-sm font-normal text-green-500">
+            A new verification link has been sent to the email address you provided during
+            registration.
+        </p>
+        @endif
     </div>
-
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="bg-purple-800">
-        <div class="max-w-7xl mx-auto py-5 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
-            <div class="max-w-4xl mx-auto text-center">
-                <h2 class="text-1xl font-bold text-white sm:text-4xl">
-                    The service you deserve, with the people you trust.
-                </h2>
-                <p class="mt-3 text-xl text-indigo-200 sm:mt-4">
-                    Simplify your rental operations with The PMO
-                </p>
-            </div>
-            <dl class="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
-                <div class="flex flex-col">
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
-                        Customer Support
-                    </dt>
-                    <dd class="order-1 text-5xl font-extrabold text-white">
-                        24/7
-                    </dd>
-                </div>
-
-                <div class="flex flex-col mt-10 sm:mt-0">
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
-                        Statistics and Reports
-                    </dt>
-                    <dd class="order-1 text-5xl font-extrabold text-white">
-                        Real-time
-                    </dd>
-                </div>
-                <div class="flex flex-col mt-10 sm:mt-0">
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
-                        Properties
-                    </dt>
-                    <dd class="order-1 text-5xl font-extrabold text-white">
-                        Unlimited
-                    </dd>
-                </div>
-            </dl>
-        </div>
-    </div>
-    <!-- This example requires Tailwind CSS v2.0+ -->
-
-
-</x-checkout-base-component>
+    </x-newlayout-base>
