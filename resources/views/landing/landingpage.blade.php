cb281dda1bcd2b691124718bdc6fa4487aedfe42
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-09-23 11:19:41 +0800

    updated landing page

--- resources/views/landing/landingpage.blade.php
+++ resources/views/landing/landingpage.blade.php
@@ -275,7 +275,7 @@
             
 
             <div>
-              <button type="submit" class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Start Free Trial</button>
+              <button type="submit" class="w-48 flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Start Free Trial</button>
             </div>
 
           
