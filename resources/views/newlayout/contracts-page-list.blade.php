da6088b7f4ada3de9284b9bd1642c5f9fa4eed86
arpct1441 <thepropertymanagernoreply@gmail.com>
2022-09-23 23:05:35 +0800

    updated contracts

--- resources/views/newlayout/contracts-page-list.blade.php
+++ resources/views/newlayout/contracts-page-list.blade.php
@@ -309,11 +309,12 @@
   }
   ```
 -->
-<div class="mt-10 px-4 sm:px-6 lg:px-8">
+<div class="mt-5 px-4 sm:px-6 lg:px-8">
   <div class="sm:flex sm:items-center">
     <div class="sm:flex-auto">
     
       <h1 class="mb-5 text-3xl font-bold text-gray-700">Contracts</h1>
+      
     
    
   </div>
@@ -324,6 +325,9 @@
   <div class="sm:col-span-3">
 
   <div class="sm:col-span-2">
+  <div class="flex justify-end py-2">
+<button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-900 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"><a href="newunits_thumbnail">Add new Contract</a></button>
+                  </div>
 <form>   
     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300 ">Filters</label>
     <div class="relative w-64 mb-5">
@@ -470,9 +474,7 @@
 
     
 </form>
-                  
 
-    
     
 </div>
 
