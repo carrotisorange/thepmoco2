8aa02cf17d663e274c68f11e999b4c789db940bd
= <=>
2022-09-24 18:12:47 +0800

    improved the contracts page.

--- resources/views/admin/tables/contracts.blade.php
+++ resources/views/admin/tables/contracts.blade.php
@@ -1,55 +1,63 @@
-<table class="min-w-full table-fixed divide-y-8 divide-gray-50 border">
-    <thead class="bg-gray-50">
+<table class="min-w-full table-fixed">
+    <thead class="">
         <tr>
-            <x-th>Tenant</x-th>
-            <x-th>Unit</x-th>
-            <x-th>Duration</x-th>
-            <x-th>Rent/Mo</x-th>
-            <x-th>Status</x-th>
-            <x-th>Interaction</x-th>
-            <x-th></x-th>
-            <x-th></x-th>
+            <th scope="col" class="relative w-12 px-6 sm:w-16 sm:px-8">
+            
+            </th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">
+                #</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">TENANT</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">UNIT</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">BUILDING</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">DURATION
+            </th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">RENT/MO</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">STATUS</th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">INTERACTION
+            </th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"></th>
+            <th scope="col" class="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"></th>
         </tr>
     </thead>
-    @forelse ($contracts as $item)
-    <tbody class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+    @forelse ($contracts as $index => $item)
+    <tbody class=" divide-gray-50 border divide-y gap-y-6 bg-white">
         <tr>
-            <x-td>
+            <td class="relative w-12 px-6 sm:w-16 sm:px-8">
+                <!-- Selected row marker, only show when row is selected. -->
+            
+                {{-- <input type="checkbox"
+                    class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6">
+                --}}
+            </td>
+            <td class="whitespace-nowrap py-4 pr-3 text-sm font-medium text-gray-900">
+                {{ $index+1 }}
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 <div class="text-sm text-gray-900">
                     @if(auth()->user()->role_id == '8')
                     {{ $item->tenant->tenant }}
                     @else
-                    <a class="text-blue-800 font-bold"
+                    <a class="text-blue-500 text-decoration-line: underline"
                         href="/property/{{ Session::get('property') }}/tenant/{{ $item->tenant->uuid }}">
-                        {{ $item->tenant->tenant }}
+                      {{ $item->tenant->tenant }}
                     </a>
                     @endif
-
                 </div>
-
-                <div class="text-sm text-gray-500">{{
-                    $item->tenant->type}}
-                </div>
-
-            </x-td>
-            <x-td>
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 @if(auth()->user()->role_id == '8')
                 {{ $item->unit->unit }}
                 @else
                 <div class="text-sm text-gray-900">
-                    <a class="text-blue-800 font-bold"
+                    <a class="text-blue-500 text-decoration-line: underline"
                         href="/property/{{ Session::get('property') }}/unit/{{ $item->unit->uuid }}">
-                        {{ $item->unit->unit }}
+                       {{ $item->unit->unit }}
                     </a>
+                 
                 </div>
                 @endif
-
-                <div class="text-sm text-gray-500">{{
-                    $item->unit->building->building}}
-                </div>
-
-            </x-td>
-            <x-td>
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 <div class="text-sm text-gray-900">{{
                     Carbon\Carbon::parse($item->start)->format('M d, Y').' -
                     '.Carbon\Carbon::parse($item->end)->format('M d, Y') }}
@@ -60,9 +68,9 @@
                     }} months
                 </div>
 
-            </x-td>
-            <x-td>{{number_format($item->rent, 2)}}</x-td>
-            <x-td>
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{number_format($item->rent, 2)}}</td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 @if($item->status === 'active')
                 <span class="px-2 text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                     <i class="fa-solid fa-circle-check"></i> {{ $item->status }}
@@ -83,21 +91,25 @@
                         <i class="fa-solid fa-clock"></i> expiring
                     </span>
                     @endif
-            </x-td>
-            <x-td>{{ $item->interaction->interaction }}</x-td>
-            <x-td>
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $item->interaction->interaction }}</td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 <a href="/property/{{ $item->property_uuid }}/tenant/{{ $item->tenant_uuid }}/contract/{{ $item->uuid }}/renew"
                     class="text-indigo-600 hover:text-indigo-900">Renew</a>
-            </x-td>
-            <x-td>
+            </td>
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                 @if($item->status == 'active' || $item->status == 'pendingmoveout')
                 <a href="/property/{{ $item->property_uuid }}/tenant/{{ $item->tenant_uuid }}/contract/{{ $item->uuid }}/moveout"
                     class="text-indigo-600 hover:text-indigo-900">Moveout</a>
                 @endif
 
-            </x-td>
-            @empty
-            <x-td>No data found!</x-td>
+            </td>
+
+        </tr>
+        @empty
+        <tr>
+
+            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">No data found!</td>
         </tr>
         @endforelse
     </tbody>
