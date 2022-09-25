c715ef4e75931a2d17d6d78129055c2bb6c85a4f
= <=>
2022-09-25 10:40:07 +0800

    Update unit-component.blade.php

--- resources/views/livewire/unit-component.blade.php
+++ resources/views/livewire/unit-component.blade.php
@@ -15,7 +15,32 @@
         </div>
         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
 
+            <button  class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="dropdownButton" data-dropdown-toggle="unitsOptionsDropdown" type="button">Add <svg
+                    class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7">
+                    </path>
+                </svg></button>
 
+            <div id="unitsOptionsDropdown"
+                class="text-left hidden z-10 w-30 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
+                <ul class="py-1" aria-labelledby="unitsOptionsDropdown">
+
+                    <li>
+                        <a href="#/" data-modal-toggle="create-unit-modal"
+                            class=" block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            Unit
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#/" data-modal-toggle="add-building-modal"
+                            class=" block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            Building
+                        </a>
+                    </li>
+                </ul>
+            </div>
             @if($selectedUnits)
             <button type="submit" wire:click="removeUnits()"
                 class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
