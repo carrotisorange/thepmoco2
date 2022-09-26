79b0042cf7abf2af800ec3277dbc6ab414322967
= <=>
2022-09-26 10:56:54 +0800

    improved cashflow

--- app/Http/Livewire/CashflowIndexComponent.php
+++ app/Http/Livewire/CashflowIndexComponent.php
@@ -8,30 +8,12 @@ use Session;
 
 class CashflowIndexComponent extends Component
 {
-    public $filter = 'daily';
+    public $filter='daily';
 
     public function render()
     {
-        if($this->filter == 'daily')
-        {
-             $collections = DB::table('acknowledgement_receipts')
-             ->select('id',DB::raw('DATE_FORMAT(created_at, "%d-%b-%Y") as date') ,DB::raw('sum(amount) as amount'),
-             DB::raw("'INCOME' as label"))
-             ->where('property_uuid', Session::get('property'))
-             ->groupBy('date')
-             ->orderBy('created_at', 'desc')
-             ->get();
-
-             $accountpayables = DB::table('account_payables')
-             ->select('id',DB::raw('DATE_FORMAT(created_at, "%d-%b-%Y") as date') ,DB::raw('sum(amount) as amount'),
-             DB::raw("'EXPENSE' as label"))
-             ->where('property_uuid', Session::get('property'))
-             ->where('status', 'approved')
-             ->groupBy('date')
-             ->orderBy('created_at', 'desc')
-             ->get();
-
-        }elseif($this->filter == 'monthly')
+  
+        if($this->filter == 'monthly')
         {
             $collections = DB::table('acknowledgement_receipts')
             ->select('id',DB::raw('DATE_FORMAT(created_at, "%b-%Y") as date') ,DB::raw('sum(amount) as amount'),
@@ -49,7 +31,8 @@ class CashflowIndexComponent extends Component
             ->groupBy('date')
             ->orderBy('created_at', 'desc')
             ->get();
-        }else{
+
+        }elseif($this->filter == 'yearly'){
              $collections = DB::table('acknowledgement_receipts')
              ->select('id',DB::raw('DATE_FORMAT(created_at, "%Y") as date') ,DB::raw('sum(amount) as amount'),
              DB::raw("'INCOME' as label"))
@@ -60,6 +43,24 @@ class CashflowIndexComponent extends Component
 
              $accountpayables = DB::table('account_payables')
              ->select('id',DB::raw('DATE_FORMAT(created_at, "%Y") as date') ,DB::raw('sum(amount) as amount'),
+             DB::raw("'EXPENSE' as label"))
+             ->where('property_uuid', Session::get('property'))
+             ->where('status', 'approved')
+             ->groupBy('date')
+             ->orderBy('created_at', 'desc')
+             ->get();
+        }else
+        {
+             $collections = DB::table('acknowledgement_receipts')
+             ->select('id',DB::raw('DATE_FORMAT(created_at, "%d-%b-%Y") as date') ,DB::raw('sum(amount) as amount'),
+             DB::raw("'INCOME' as label"))
+             ->where('property_uuid', Session::get('property'))
+             ->groupBy('date')
+             ->orderBy('created_at', 'desc')
+             ->get();
+
+             $accountpayables = DB::table('account_payables')
+             ->select('id',DB::raw('DATE_FORMAT(created_at, "%d-%b-%Y") as date') ,DB::raw('sum(amount) as amount'),
              DB::raw("'EXPENSE' as label"))
              ->where('property_uuid', Session::get('property'))
              ->where('status', 'approved')
--- resources/views/livewire/cashflow-index-component.blade.php
+++ resources/views/livewire/cashflow-index-component.blade.php
@@ -36,7 +36,7 @@
             <div class="sm:col-span-2">
                 <select id="small" wire:model="filter"
                     class="text-left bg-white block p-1 w-full text-sm h-8 text-gray-90 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
-                    <option value="">Filter cashflow</option>
+                    
                     <option value="daily">Daily</option>
                     <option value="monthly">Monthly</option>
                     <option value="yearly">Yearly</option>
