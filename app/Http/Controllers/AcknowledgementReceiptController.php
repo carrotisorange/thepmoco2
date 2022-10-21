e3b0a42239c69c007bdb3fef5155352ee70d0401
CTO <thepropertymanager2020@gmail.com>
2022-10-21 10:51:01 +0800

    fixed errors on xendit sms link

--- app/Http/Controllers/AcknowledgementReceiptController.php
+++ app/Http/Controllers/AcknowledgementReceiptController.php
@@ -10,6 +10,8 @@ class AcknowledgementReceiptController extends Controller
 {
     public function index(Property $property)
     {
+        $this->authorize('is_account_receivable_read_allowed');
+
         app('App\Http\Controllers\ActivityController')->store($property->uuid, auth()->user()->id,'opens',11);
         
         $collections = Property::find(Session::get('property'))->acknowledgementreceipts;
