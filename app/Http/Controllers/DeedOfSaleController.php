daae88b7c16e051280bacba5215f31e10faeb177
CTO <thepropertymanager2020@gmail.com>
2022-10-28 19:05:04 +0800

    defined restrictions on deed of sale as part of the owner portal

--- app/Http/Controllers/DeedOfSaleController.php
+++ app/Http/Controllers/DeedOfSaleController.php
@@ -12,11 +12,15 @@ class DeedOfSaleController extends Controller
 {
     public function show_unit_deed_of_sales($unit_uuid)
     {
+        $this->authorize('is_owner_portal_read_allowed');
+
         return Unit::findOrFail($unit_uuid)->deed_of_sales()->paginate(5);
     }
 
     public function create(Property $property, Unit $unit, Owner $owner)
     {
+        $this->authorize('is_owner_portal_create_allowed');
+
         return view('deed_of_sales.create',[
             'unit' => $unit,
             'owner' => $owner
@@ -24,6 +28,8 @@ class DeedOfSaleController extends Controller
     }
 
     public function edit(Property $property, Unit $unit, Owner $owner, DeedOfSale $deedOfSale){
+        $this->authorize('is_owner_portal_update_allowed');
+
         return view('deed_of_sales.edit',[
             'deedOfSale' => $deedOfSale
         ]);
