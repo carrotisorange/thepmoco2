03c7614047b3d3dc002559c7c9b22ef586754182
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2022-12-06 18:19:55 +0800

    fixed submit button on click

--- resources/views/landing/demopage.blade.php
+++ resources/views/landing/demopage.blade.php
@@ -145,7 +145,7 @@ document.addEventListener('DOMContentLoaded', function() {
 <!-- Code block starts -->
 <dh-component>
             
-            <form action="" method="POST">
+            <form id="demoForm" action="" method="POST">
             <div class="py-12  transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
                 <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
                     <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
@@ -168,7 +168,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         </div>
                         <p class="pb-10 text-xs font-light text-gray-600">By submitting my personal information, I understand and agree that The PMO may collect, process, and retain my data pursuant to The PMO Co. <span class="text-purple-900 font-semibold"><a href="privacy">Privacy Policy.</a></span></p>
                         <div class="flex items-center justify-start w-full">
-                            <button type ="submit" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-purple-600 bg-purple-500 rounded-2xl text-white px-8 py-2 text-sm">Submit</button>
+                            <button type ="submit" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-purple-600 bg-purple-500 rounded-2xl text-white px-8 py-2 text-sm" onclick="modalHandler()">Submit</button>
                             <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded-2xl px-8 py-2 text-sm"><a href="/">Cancel</a></button>
                         </div>
                        
@@ -205,6 +205,8 @@ document.addEventListener('DOMContentLoaded', function() {
                             requestAnimationFrame(fade);
                         }
                     })();
+
+                    
                 }
             </script>
             
