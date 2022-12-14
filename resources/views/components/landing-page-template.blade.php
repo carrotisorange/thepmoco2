5ac1b18c2edfd8230f54062115081555269dbca7
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2022-12-14 23:14:42 +0800

    added google tags for analytics

--- resources/views/components/landing-page-template.blade.php
+++ resources/views/components/landing-page-template.blade.php
@@ -89,6 +89,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-P3GZPSF');</script>
 <!-- End Google Tag Manager -->
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XFVDKSHJQL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XFVDKSHJQL');
+</script>
 
 </head>
 
