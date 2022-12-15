f3bcdaa7115532b9563a61aa7ded5f5bc58c2078
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2022-12-15 18:22:42 +0800

    added step 1 and step 2 of request for purchase

--- resources/views/accountpayables/create/step-1.blade.php
+++ resources/views/accountpayables/create/step-1.blade.php
@@ -2,12 +2,56 @@
     @section('title','Step 1 of Step 6 | Account Payables')
     <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="mt-5 px-4 sm:px-6 lg:px-8">
+        {{-- stepper --}}
+            
+
             {{-- start-step-1-form --}}
             <form class="space-y-6" wire:submit.prevent="submitForm()" method="POST">
+            
+                <div class="md:grid md:grid-cols-6 md:gap-6">
 
+                     {{-- request for purchase --}}
+                    <div class="sm:col-span-3">
+                        <label for="request" class="block text-sm font-medium text-gray-700">Request for: </label>
+                            <select id="purchase" name="purchase" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block h-8 w-full sm:text-sm border border-gray-700  rounded-md">
+                                <option>Purchase</option>
+                            </select>
+                    </div>
 
+                    {{-- creation date --}}
+                    <div class="sm:col-span-3">
+                        <label for="creation-date" class="block text-sm font-medium text-gray-700">Document Date:</label>
+                            <select id="creation-date" name="creation-date" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block h-8 w-full sm:text-sm border border-gray-700  rounded-md">
+                                <option>Date</option>
+                            </select>
+                    </div>
+
+                    {{-- particular --}}
+                    <div class="sm:col-span-3">
+                        <label for="particular" class="block text-sm font-medium text-gray-700">Particular:</label>
+                            <select id="particular" name="particular" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block h-8 w-full sm:text-sm border border-gray-700  rounded-md">
+                                <option>Rent</option>
+                                <option>Rent</option>
+                                <option>Rent</option>
+                                <option>Rent</option>
+                            </select>
+                    </div>
+
+                    {{-- requester's name --}}
+                    <div class="sm:col-span-3">
+                        <label for="concern" class="block text-sm font-medium text-gray-700">Requester's Name:</label>
+                            <textarea id="concern" name="concern" rows="3" class="mt-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full h-8 sm:text-sm border border-gray-700 rounded-md"></textarea>
+                    </div>
+
+                    {{-- cancel, next button --}}
+                    <div class="col-start-6 flex items-center justify-end">
+                        <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
+                        <button type="submit" class="ml-3 inline-flex justif y-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><a href="accountspayables2">Next</a></button>
+                    </div>
+
+                </div>
             </form>
             {{-- end-step-1-form --}}
-        </div>
+        </div>          
     </div>
 </x-new-layout>
