0f12b302620ad9c6fd43a13e2e90f598576b80e0
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2022-12-19 22:40:20 +0800

    removed background image, fixed tabs display of tenant

--- resources/views/tenants/show.blade.php
+++ resources/views/tenants/show.blade.php
@@ -1,14 +1,13 @@
 <x-new-layout>
     @section('title', $tenant_details->tenant. ' | '. Session::get('property_name'))
-    <div class="h-full w-full bg-no-repeat bg-cover" style="background-image: url('/brands/tenant_bg.png');">
-        <div class="mt-8">
+    <div class="h-full w-full mb-20">
             <div class="max-w-full mx-auto sm:px-6">
                 <div class="pt-6 sm:pb-5">
-                    <nav aria-label="Breadcrumb" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <nav aria-label="Breadcrumb" class="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
                         <ol role="list" class="flex items-center space-x-4">
                             <li>
                                 <div class="flex items-center">
-                                    <a href="/property/{{ Session::get('property')}}/tenant">
+                                    <a class=""href="/property/{{ Session::get('property')}}/tenant">
                                         <img class="h-5 w-auto" src="{{ asset('/brands/back-button.png') }}">
                                     </a>
                                 </div>
@@ -18,6 +17,5 @@
                     @livewire('tenant-edit-component', ['tenant_details' => $tenant_details])
                 </div>
             </div>
-        </div>
     </div>
 </x-new-layout>
