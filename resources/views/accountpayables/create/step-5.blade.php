f5ac78344b72df463fa64410a36b2e7cb39b7f23
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-01-07 20:24:56 +0800

    linked comment page

--- resources/views/accountpayables/create/step-5.blade.php
+++ resources/views/accountpayables/create/step-5.blade.php
@@ -245,7 +245,7 @@
 
                     {{-- reject, approve button --}}
                     <div class="col-start-6 flex items-center justify-end">
-                    <button type="button" class="ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Comment</button>
+                        <a href="request-comment"><button type="button" class="ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Comment</button></a>
                         <button type="button" class="ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Reject</button>
                         <button type="submit" class="ml-3 inline-flex justif y-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><a href="accountspayables2">Approve</a></button>
                     </div>
