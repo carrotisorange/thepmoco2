59280dc830ca11c85d2d2122a1e66cbdc5523f8a
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-01-13 12:08:30 +0800

    arranged articles from latest to oldest

--- resources/views/landing/blog-1.blade.php
+++ resources/views/landing/blog-1.blade.php
@@ -27,19 +27,20 @@
   <div class="relative mx-auto max-w-6xl">
     
     <div class="mx-auto mt-3 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-4">
-      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+
+    <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
         <div class="flex-shrink-0">
-          <a href="article1"><img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="a desk with a keyboard, mouse, phone, and office supplies"></a>
+          <a href="article6"><img class="h-48 w-full object-cover" src="{{ asset('/brands/landing/article6.jpg') }}" alt="three people looking laptop property management system"></a>
         </div>
         <div class="flex flex-1 flex-col justify-between bg-white p-6">
           <div class="flex-1">
             <p class="text-sm font-medium text-indigo-600">
-              <a href="article1" class="hover:underline">Article</a>
+              <a href="article6" class="hover:underline">Article</a>
             </p>
-            <a href="article1" class="mt-2 block">
-              <p class="text-base font-semibold text-gray-900">How our journey started</p>
+            <a href="article6" class="mt-2 block">
+              <p class="text-base font-semibold text-gray-900">Why Digitalization is the best strategy for 2023</p>
               <p class="mt-3 text-sm text-gray-500">
-                When we first started out as property managers, we followed the old-school methods. During leasing procedures, we used the traditional way</p>
+              Digitalization is a strategic approach to business that seeks to leverage the power of digital technologies to achieve business objectives.</p>
             </a>
           </div>
           
@@ -48,36 +49,17 @@
 
       <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
         <div class="flex-shrink-0">
-          <a href="article2"><img class="h-48 w-full object-cover" src="{{ asset('/brands/landing/article2.jpg') }}" alt="three people looking laptop property management system"></a>
+          <a href="article5"><img class="h-48 w-full object-cover" src="{{ asset('/brands/landing/article5.jpg') }}" alt="three people looking laptop property management system"></a>
         </div>
         <div class="flex flex-1 flex-col justify-between bg-white p-6">
           <div class="flex-1">
             <p class="text-sm font-medium text-indigo-600">
-              <a href="article2" class="hover:underline">Article</a>
+              <a href="article5" class="hover:underline">Article</a>
             </p>
-            <a href="article2" class="mt-2 block">
-              <p class="text-base font-semibold text-gray-900">How to use a leasing management system to improve operational efficiency?</p>
+            <a href="article5" class="mt-2 block">
+              <p class="text-base font-semibold text-gray-900">What do we offer as a SaaS company to property managers and owners in The Property Manager Online?</p>
               <p class="mt-3 text-sm text-gray-500">
-              If you've ever tried to manage a property, you know how hard it can be. From accounting for rent payments to tracking expenses, there are just so many things that need to get done.</p>
-            </a>
-          </div>
-          
-        </div>
-      </div>
-
-      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
-        <div class="flex-shrink-0">
-          <a href="article3"><img class="h-48 w-full object-cover" src="{{ asset('/brands/landing/article3.jpg') }}" alt="three people looking laptop property management system"></a>
-        </div>
-        <div class="flex flex-1 flex-col justify-between bg-white p-6">
-          <div class="flex-1">
-            <p class="text-sm font-medium text-indigo-600">
-              <a href="article3" class="hover:underline">Article</a>
-            </p>
-            <a href="article3" class="mt-2 block">
-              <p class="text-base font-semibold text-gray-900">What are the benefits of a property management system for property managers?</p>
-              <p class="mt-3 text-sm text-gray-500">
-              A property management system can be a great tool for landlords and property managers. It can help you to manage your properties more efficiently,</p>
+              Property managers, real estate agents, and owners of property are always looking for ways to improve the efficiency with which they do their jobs.</p>
             </a>
           </div>
           
@@ -99,10 +81,29 @@
               In today's marketplace, it's hard for small businesses to stand out from the crowd. But there is one thing that every small business has in common:</p>
             </a>
           </div>
-          
         </div>
       </div>
 
+      <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+        <div class="flex-shrink-0">
+          <a href="article3"><img class="h-48 w-full object-cover" src="{{ asset('/brands/landing/article3.jpg') }}" alt="three people looking laptop property management system"></a>
+        </div>
+        <div class="flex flex-1 flex-col justify-between bg-white p-6">
+          <div class="flex-1">
+            <p class="text-sm font-medium text-indigo-600">
+              <a href="article3" class="hover:underline">Article</a>
+            </p>
+            <a href="article3" class="mt-2 block">
+              <p class="text-base font-semibold text-gray-900">What are the benefits of a property management system for property managers?</p>
+              <p class="mt-3 text-sm text-gray-500">
+              A property management system can be a great tool for landlords and property managers. It can help you to manage your properties more efficiently,</p>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      
+        
 
     </div>
   </div>
