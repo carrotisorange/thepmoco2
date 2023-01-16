aa0eada2c2f13efdb353944b6be6709320a136fc
CTO <thepropertymanager2020@gmail.com>
2023-01-16 13:48:49 +0800

    Update UnitBillController.php

--- app/Http/Controllers/UnitBillController.php
+++ app/Http/Controllers/UnitBillController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Unit;
 use App\Models\Property;
 use App\Models\Utility;
+use App\Models\Contract;
+use App\Models\DeedOfSale;
+use Session;
+use Illuminate\Support\Str;
 
 class UnitBillController extends Controller
 {
@@ -24,6 +28,20 @@ class UnitBillController extends Controller
     }
 
     public function create(Property $property, Unit $unit, $type, Utility $utility){
+
+        if($type === 'tenant'){
+            $contracts = Contract::where('unit_uuid', $unit->uuid)->where('status', 'active')->count();
+
+            if($contracts <= 0){
+                return redirect("/property/".Session::get('property').'/unit/'.$unit->uuid.'/tenant/'.Str::random(8).'/create');
+            }   
+        }elseif($type==='owner'){
+            $deedofsales = DeedOfSale::where('unit_uuid', $unit->uuid)->where('status', 'active')->count();
+
+            if($deedofsales <=0){
+                return redirect("/property/".Session::get('property').'/unit/'.$unit->uuid.'/owner/'.Str::random(8).'/create');
+            }
+        }
         return view('units.bills.create',[
             'property' => $property,
             'unit' => $unit, 
