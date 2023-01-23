709eaf937ad1e8b458c887193cc333b6eb602108
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-01-23 18:29:27 +0800

    improved article display

--- resources/views/landing/articles/article8.blade.php
+++ resources/views/landing/articles/article8.blade.php
@@ -258,7 +258,185 @@ document.addEventListener('DOMContentLoaded', function() {
     </div>
     
     <!-- start article -->
+    <div class="sm:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
 
+<div class="col-span-1">
+  <div class="bg-gray-100 p-2 hidden lg:block lg:visible ml-10 mt-10 max-w-xl">
+
+    <div class="my-5">
+      <a class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1 flex justify-center ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-center text-gray-700 w-7 h-7">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+    </svg>
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1  text-gray-700 text-lg font-bold">Read more articles:</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Article 9 -->
+    <div class="my-5">
+      <a href="article9" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+        <img src="{{ asset('/brands/landing/article9.jpg') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">How to reduce digitalization pain points</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Article 8 -->
+    <div class="my-5">
+      <a href="article7" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+          <img src="{{ asset('/brands/landing/article7.jpg') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">How To Improve Tenant Retention Rates</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+        <a href="article6" class="grid grid-cols-4 rounded-sm">
+
+          <!-- Icon -->
+          <div class="col-span-1">
+            <img src="{{ asset('/brands/landing/article6.jpg') }}" class="h-full w-full">
+          </div>
+    
+          <!-- Title -->
+          <div class="col-span-3">
+            <p class="ml-1 underline text-gray-700 text-xs">Why Digitalization is the best strategy for 2023</p>
+          </div>
+    
+
+        </a>
+      </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+        <a href="article5" class="grid grid-cols-4 rounded-sm">
+
+          <!-- Icon -->
+          <div class="col-span-1">
+            <img src="{{ asset('/brands/landing/article5.jpg') }}" class="h-full w-full">
+          </div>
+    
+          <!-- Title -->
+          <div class="col-span-3">
+            <p class="ml-1 underline text-gray-700 text-xs">What do we offer as a SaaS company to property managers and owners in The Property Manager Online?</p>
+          </div>
+    
+
+        </a>
+      </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+      <a href="article4" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+          <img src="{{ asset('/brands/landing/article4.jpg') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">Secret Recipe For Brand Building As Small Business.</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+      <a href="article3" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+          <img src="{{ asset('/brands/landing/article3.jpg') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">What are the benefits of a property management system for property managers?</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+      <a href="article2" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+          <img src="{{ asset('/brands/landing/article2.jpg') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">How to use a leasing management system to improve operational efficiency?</p>
+        </div>
+  
+
+      </a>
+    </div>
+
+    <!-- Card 1 -->
+    <div class="my-5">
+      <a href="article1" class="grid grid-cols-4 rounded-sm">
+
+        <!-- Icon -->
+        <div class="col-span-1">
+          <img src="{{ asset('/brands/landing/article1.avif') }}" class="h-full w-full">
+        </div>
+  
+        <!-- Title -->
+        <div class="col-span-3">
+          <p class="ml-1 underline text-gray-700 text-xs">How Our Journey Started</p>
+        </div>
+  
+
+      </a>
+    </div>
+  
+
+
+
+
+
+
+
+</div>
+</div>
+
+<div class="sm:col-span-1 md:col-span-2 lg:col-span-3">
       <div class="mt-10 mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style="height: 24em;">
         
         <img src="{{ asset('/brands/landing/article8.jpg') }}" alt="property management system" class="opacity-25 absolute left-0 top-0 w-full h-full z-0 object-cover" />
@@ -312,7 +490,7 @@ best.</li>
       
     
       </div>
-
+        </div>
 
 <!-- end article -->
 </main>
