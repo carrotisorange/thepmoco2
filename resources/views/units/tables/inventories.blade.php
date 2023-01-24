35fd5ad44be412e0d4893ea53fc771691398c58d
CTO <thepropertymanager2020@gmail.com>
2023-01-24 13:29:52 +0800

    added id column in unit_inventories table

--- resources/views/units/tables/inventories.blade.php
+++ resources/views/units/tables/inventories.blade.php
@@ -39,7 +39,7 @@
     <h3 class="mt-2 text-sm font-medium text-gray-900">No inventories</h3>
     <p class="mt-1 text-sm text-gray-500">You're almost there!</p>
     <div class="mt-6">
-        <button type="button" disabled
+        <button type="button"
             onclick="window.location.href='/property/{{ Session::get('property') }}/unit/{{ $unit_details->uuid }}/inventory/{{ Str::random(8) }}/create'"
             class="inline-flex items-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
             <!-- Heroicon name: mini/plus -->
