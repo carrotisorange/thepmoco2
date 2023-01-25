3abad49ea2c0525fe44c67853248f32aa300ec7b
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-01-25 14:11:11 +0800

    modified 404 page

--- resources/views/layouts/not-found.blade.php
+++ resources/views/layouts/not-found.blade.php
@@ -1,7 +1,7 @@
-<x-new-layout>
+<x-landing-page-template>
     @section('title', 'Page not found')
     
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mt-10 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
 
   <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 mt-5">
 
@@ -16,21 +16,12 @@
     <p class="text-center font-semibold text-lg text-gray-700">Page not Found.</p>
     <p class="mt-2 text-center font-light text-medium text-gray-700">Sorry, we couldn’t find the page you’re looking for.</p>
     <div class="mt-5 flex items-center justify-center">
-    <div button type="button" class="mr-3 w-72 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white text-center bg-purple-900 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"><a href="check-page">Go back.</a></button></div>
+    <a href="/"><div button type="button" class="mr-3 w-72 items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white text-center bg-purple-900 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Go back to home.</button></div></a>
 
 </div>
-            {{-- <footer class="flex-shrink-0 bg-gray-50">
-                <div class="mx-auto w-full max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
-                    <nav class="flex space-x-4">
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-gray-600">Contact Support</a>
-                        <span class="inline-block border-l border-gray-300" aria-hidden="true"></span>
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-gray-600">Status</a>
-                        <span class="inline-block border-l border-gray-300" aria-hidden="true"></span>
-                        <a href="#" class="text-sm font-medium text-gray-500 hover:text-gray-600">Twitter</a>
-                    </nav>
-                </div> --}}
-            </footer>
+            
+            
         </div>
         
     </div>
-</x-new-layout>
+</x-landing-page-template>
