f2604463c184d7f34b16029194868561e97ff7be
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-01-28 17:53:09 +0800

    update inventory

--- resources/views/livewire/unit-show-component.blade.php
+++ resources/views/livewire/unit-show-component.blade.php
@@ -197,10 +197,10 @@
 
                                     </div>
                                     @if($inventories->count())
-                                    <div class="p-4">
+                                    <div class="p-4 flex justify-end">
                                         <button type="button" wire:loading.remove
                                             wire:click="redirectToTheCreateUnitInventoryPage"
-                                            class="flex justify-end items-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
+                                            class="rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
 
                                             Update Inventory
                                         </button>
