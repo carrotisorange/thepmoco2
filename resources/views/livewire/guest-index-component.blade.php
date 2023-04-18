592ea79d4ff872ee0e1a10263b2239bce7aac2da
CTO <thepropertymanager2020@gmail.com>
2023-04-18 16:27:15 +0800

    Update guest-index-component.blade.php

--- resources/views/livewire/guest-index-component.blade.php
+++ resources/views/livewire/guest-index-component.blade.php
@@ -43,10 +43,10 @@
                     <select id="status" wire:model="status"
                         class="text-left bg-white block p-1 w-full text-sm h-8 text-gray-90 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
                         <option value="">Filter status</option>
-                        <option value="active">Active</option>
+                        <option value="checked-in">Checked-in</option>
+                        <option value="checked-out">Checked-out</option>
                         <option value="cancelled">Cancelled</option>
-                        <option value="pending">Pending</option>
-                        <option value="inactive">Inactive</option>
+                        <option value="reserved">Reserved</option>
 
                     </select>
 
