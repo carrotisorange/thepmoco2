8a3f15c950ce6359f3d639f8cab6e0cacb9f11e6
CTO <thepropertymanager2020@gmail.com>
2023-05-17 21:09:52 +0800

    fixed error in unit inventory

--- resources/views/livewire/unit-show-component.blade.php
+++ resources/views/livewire/unit-show-component.blade.php
@@ -206,7 +206,7 @@
                                     </div>
                                     @if($inventories->count())
                                     <div class="p-4 flex justify-end">
-                                        <button type="button" wire:loading.remove
+                                        <button type="button" wire:loading.remove onclick="window.location.href='/property/{{ Session::get('property') }}/unit/{{ $unit_details->uuid }}/inventory/{{ Str::random(8) }}/create'"
                                             
                                             class="rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2">
 
