fce5882bb65ae0e7fe4f93204f6e251a00491f8e
arpct1441 <78124407+arpct1441@users.noreply.github.com>
2023-05-31 19:44:29 +0800

    improved columns in property financials

--- resources/views/livewire/financial-index-component.blade.php
+++ resources/views/livewire/financial-index-component.blade.php
@@ -173,13 +173,13 @@
         </div>
  --}}
 
- <div class="py-4 px-4 sm:px-6 lg:px-8">
+ <div class="py-8 px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
                 <h1 class="text-xl font-semibold text-gray-900">Property Finanacial Reporting</h1>
 
             </div>
-            <!-- <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                 <button type="button" wire:click="downloadFinancialReports" wire:loading.remove
                     class="inline-flex items-center justify-center rounded-md border border-transparent bg-purple-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 sm:w-auto">
                     <i class="fa-solid fa-download"></i> &nbsp
@@ -192,9 +192,9 @@
                     Loading...
 
                 </button>
-            </div> -->
+            </div> 
         </div>
-        <div class="mt-8 flex flex-col">
+        <div class="mt-8 flex ">
             <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                     <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
