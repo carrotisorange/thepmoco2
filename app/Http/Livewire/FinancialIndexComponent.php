96d6a5a23e9a6f77831b09a3c811c337c11b8aff
CTO <thepropertymanager2020@gmail.com>
2023-06-16 12:56:29 +0800

    added the revenues display on the financials

--- app/Http/Livewire/FinancialIndexComponent.php
+++ app/Http/Livewire/FinancialIndexComponent.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Collection;
+use DB;
+use Carbon\Carbon;
 
 class FinancialIndexComponent extends Component
 {
@@ -50,9 +52,38 @@ class FinancialIndexComponent extends Component
 
         $actual_revenue_collected = app('App\Http\Controllers\PropertyFinancialController')->get_actual_revenue_collected($this->property);
 
-        //financial reports
-        $revenues = Collection::where('property_uuid', $this->property->uuid)->get();
 
+// SELECT
+// collections.bill_id AS Collection_ID,
+// bills.id AS Bills_ID,
+// particulars.particular,
+// sum(collections.collection) AS Collections
+// FROM collections join bills join particulars
+// ON collections.bill_id = bills.id AND bills.particular_id = particulars.id
+// WHERE collections.property_uuid = 'c0a3db6c-6c2e-47c2-8142-c6ffc9567c92'
+// AND collections.updated_at >= '2022-01-01' AND collections.updated_at <= '2023-01-01' GROUP BY particulars.id;
+
+        //financial reports
+        // $revenues = Collection::where('property_uuid', $this->property->uuid)->get();
+
+        $revenues = DB::table('collections')
+        ->select(DB::raw("SUM(collections.collection) as amount"), 'particulars.particular as particular')
+        ->join('bills','collections.bill_id', 'bills.id')
+        ->join('particulars', 'bills.particular_id', 'particulars.id')
+        ->where('collections.property_uuid', $this->property->uuid)
+        ->whereYear('collections.updated_at', Carbon::now()->format('Y'))
+        ->groupBy('bills.particular_id')
+        ->orderBy('amount', 'desc')
+        ->get();
+
+        // $expenses = DB::table('liquidations')
+        //   ->select(DB::raw("SUM(collections.collection) as amount"), 'particulars.particular as particular')
+        //   ->join('bills','collections.bill_id', 'bills.id')
+        //   ->join('particulars', 'bills.particular_id', 'particulars.id')
+        //   ->where('collections.property_uuid', $this->property->uuid)
+        //   ->whereYear('collections.updated_at', Carbon::now()->format('Y'))
+        //   ->groupBy('bills.particular_id')
+        //   ->get();
 
         return view('livewire.financial-index-component',[
               'cashflows' => $cashflows,
@@ -63,7 +94,8 @@ class FinancialIndexComponent extends Component
               'effective_gross_rent' => $effective_gross_rent,
               'collected_rent' => $collected_rent, 
               'billed_rent' => $billed_rent,
-              'actual_revenue_collected' => $actual_revenue_collected
+              'actual_revenue_collected' => $actual_revenue_collected,
+              'revenues' => $revenues
 
         ]);
     }
