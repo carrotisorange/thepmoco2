453d913edd6d64bc97454a8a92be48560d837f28
Aira Cortez <arpct1441@gmail.com>
2023-06-30 14:22:36 +0800

    added remittance in navbar, improved room page in proprent

--- resources/views/proprent/room.blade.php
+++ resources/views/proprent/room.blade.php
@@ -99,10 +99,10 @@ body {
     <!-- Static sidebar for desktop -->
     <div class=" md:fixed md:inset-y-0 right-0 md:flex md:w-96 md:flex-col ">
         <!-- Sidebar component, swap this element with another sidebar if you like -->
-        <div class="-ml-12 flex flex-grow flex-col overflow-y-auto bg-white pt-5">
+        <div class="-ml-10 mx-6 flex flex-grow flex-col overflow-y-auto bg-white pt-5">
             <div class="py-20 mt-5 flex flex-1 flex-col">
     
-                <div class="py-8 px-10 mx-auto border border-gray-400 rounded-md">
+                <div class="py-8 px-7 mx-auto border border-gray-400 rounded-md">
                     <h1 class="font-light text-sm">Room Details </h1>
           
             
@@ -112,16 +112,28 @@ body {
                         </div>
                     </div>
             
-                    <p class="mt-4 text-xs  text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non purus vitae massa sollicitudin ultrices. Aenean mollis tincidunt leo. Nunc auctor magna ut convallis gravida. Cras porttitor orci arcu, eu facilisis lacus facilisis vel.</p>
-            
+                    <p class="font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae mattis massa, in convallis ligula. Aliquam erat volutpat.</p>
+
+                    <div class="flex justify-between">
+                    <p class="mt-4 text-xs font-medium text-gray-700">Start Date: <span class="font-light" >06/30/23</span></p>
+                    <p class="mt-4 text-xs font-medium text-gray-700">End Date: <span class="font-light" >06/30/23</span></p>
+                    </div>
+
+                    <div class="flex justify-between">
+                    <p class="mt-2 text-sm font-medium text-gray-700">Security Deposit: <span class="font-light">1000</span></p>
+                    <p class="mt-2 text-sm font-medium text-gray-700">Advanced Rent: <span class="font-light">1000</span></p>
+                    </div>
+
                     <div class="grid grid-cols-2">
                         <div>
                             <p class="mt-6 font-bold text-md text-2xl"><span>₱</span> 1000</p>
+                            Total Price
                         </div>
                     <div>
     
-                    <div class="mt-6 flex justify-center items-center">
-                        <button class="text-white bg-yellow-400 p-2 rounded-lg text-sm">Message Lessor</button>
+                    <div class="mt-6 flex justify-center items-center space-x-2">
+                    <button class="text-white bg-yellow-400 p-2 rounded-lg text-sm">Reserve</button>
+                        <button class="text-white bg-yellow-400 p-2 rounded-lg text-sm">Message</button>
                     </div>
                 </div>
             
@@ -167,7 +179,7 @@ body {
 
 
  <!-- Image gallery -->
- <div class="lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
+ <div class="h-full lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
           <h2 class="sr-only">Images</h2>
 
           <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
@@ -175,7 +187,15 @@ body {
           <div class="swiper mySwiper">
             <div class="swiper-wrapper">
               <div class="swiper-slide">
-                <img class="object-cover w-full " src="{{ asset('/brands/proprent/room-sample.jpg') }}"
+                <img class="object-cover w-full h-full" src="{{ asset('/brands/proprent/room-sample.jpg') }}"
+                  alt="image" />
+              </div>
+              <div class="swiper-slide">
+                <img class="object-cover w-full h-full" src="{{ asset('/brands/proprent/room-sample.jpg') }}"
+                  alt="image" />
+              </div>
+              <div class="swiper-slide">
+                <img class="object-cover w-full h-full" src="{{ asset('/brands/proprent/room-sample.jpg') }}"
                   alt="image" />
               </div>
               <div class="swiper-slide">
@@ -220,69 +240,51 @@ body {
 
        <!-- amenities -->
 <h1 class="mb-10 inline-flex">
-<img width="26" height="26" src="https://img.icons8.com/metro/26/exterior.png" alt="exterior"/>
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/exterior.png" alt="exterior"/>
 <span class="font-light text-sm">Apartment</span>
 </h1>
 
 <h1 class="mb-10 inline-flex">
-<img width="26" height="26" src="https://img.icons8.com/metro/26/bedroom.png" alt="bedroom"/>
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/bed.png" alt="bed"/>
 <span class="font-light text-sm">2 Bedrooms</span>
 </h1>
 
-<h1 class="mb-10 flex-1">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-</svg>
-
-
+<h1 class="mb-10 inline-flex">
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/calendar--v1.png" alt="calendar--v1"/>
 <span class="font-light text-sm">0 months minimum stay</span>
 </h1>
 
-<h1 class="mb-10 flex-1">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
-</svg>
-
-
+<h1 class="mb-10 inline-flex">
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/tv.png" alt="tv"/>
 <span class="font-light text-sm">Furnished</span>
 </h1>
 
-<h1 class="mb-10 flex-1">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
-</svg>
-
-
+<h1 class="mb-10 inline-flex">
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/wi-fi-good.png" alt="wi-fi-good"/>
 <span class="font-light text-sm">Wifi</span>
 </h1>
 
-<h1 class="mb-10 flex-1">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
-</svg>
-
-
+<h1 class="mb-10 inline-flex">
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/shower-and-tub.png" alt="shower-and-tub"/>
 <span class="font-light text-sm">1 Bathroom</span>
 </h1>
 
-<h1 class="mb-10 flex-1">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
-</svg>
-
-
+<h1 class="mb-10 inline-flex">
+<img width="26" height="26" src="https://img.icons8.com/fluency-systems-regular/48/parking.png" alt="parking"/>
 <span class="font-light text-sm">Parking</span>
 </h1>
 
 
 </div>
 
-      <h1>Amenities</h1>
       <h2 class="mt-5 mb-10 text-lg font-bold">Room Details </h2> 
-      <p class="text-sm font-light">Location is near lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non purus vitae massa sollicitudin ultrices. Aenean mollis tincidunt leo. Nunc auctor magna ut convallis gravida. Cras porttitor orci arcu, eu facilisis lacus facilisis vel. Aenean pulvinar urna vel est tempor, vel tristique sem sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec fringilla tellus ligula, et ullamcorper eros ultricies nec</p>
+        <p class="text-sm font-light">Location is near lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non purus vitae massa sollicitudin ultrices. Aenean mollis tincidunt leo. Nunc auctor magna ut convallis gravida. Cras porttitor orci arcu, eu facilisis lacus facilisis vel. Aenean pulvinar urna vel est tempor, vel tristique sem sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec fringilla tellus ligula, et ullamcorper eros ultricies nec</p>
       </div>
-        
-</div>
+       
+      <div class="my-10">
+        <img class="w-full" src="https://snazzy-maps-cdn.azureedge.net/assets/232526-sample-map-style.png?v=20180925120645" alt="shower-and-tub"/> 
+      </div>
+    </div>
 
      
 
