f825809b5de268b588e9216e55f28abd97171120
Aira Cortez <arpct1441@gmail.com>
2023-08-29 13:57:03 +0800

    changed, added logos

--- resources/views/landing/landingpage.blade.php
+++ resources/views/landing/landingpage.blade.php
@@ -540,19 +540,23 @@
                 <div class="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
                   <p class="text-center text-xl font-semibold text-gray-500">Partnered with:</p>
 
-                  <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-4">
+                  <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
                     <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                      <a href="https://www.digitalocean.com/trust/certification-reports?fbclid=IwAR3qJ79YUNro9Px4ycCvcuCgGH7MVxbcrvQitnhlh4i51su4PuJsRyKr6T0"><img class="w-20 h-20" src="{{ asset('/brands/landing/digital-ocean.png') }}" alt="digital ocean logo"></a>
+                      <a href="https://www.digitalocean.com/trust/certification-reports?fbclid=IwAR3qJ79YUNro9Px4ycCvcuCgGH7MVxbcrvQitnhlh4i51su4PuJsRyKr6T0"><img class="w-20 " src="{{ asset('/brands/landing/digital-ocean.png') }}" alt="digital ocean logo"></a>
                     </div>
                     <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                      <img class="w-30 h-20" src="{{ asset('/brands/landing/aim-logo.png') }}" alt="AIM logo">
+                      <img class="w-24 " src="{{ asset('/brands/landing/aim-logo.png') }}" alt="AIM logo">
                     </div>
                     <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                      <img class="w-20 h-20" src="{{ asset('/brands/clients/client-3.png') }}" alt="martha logo">
+                    <a href="https://marthavacationhomes.com/"><img class="w-20 " src="{{ asset('/brands/clients/martha.png') }}" alt="martha logo"></a>
                     </div>
                     <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
-                      <img class="w-20 h-20" src="{{ asset('/brands/clients/client-4.png') }}" alt="cura logo">
+                    <a href="https://www.curaservices.net/"><img class="w-20 " src="{{ asset('/brands/clients/cura.png') }}" alt="cura logo"></a>
                     </div>
+                    <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
+                      <img class="w-24 " src="{{ asset('/brands/clients/bayani-hall.png') }}" alt="bayani hall logo">
+                    </div>
+    
                   </div>
                 </div>
             </div>
