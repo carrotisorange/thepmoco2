f7f6ace2da48f908bfc140c0f10b64900b2dca9e
carrotisorange <thepropertymanager2020@gmail.com>
2023-10-18 15:27:09 +0800

    improved property steppers table

---
+++ app/Models/PropertyBulletin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PropertyBulletin extends Model
+{
+    use HasFactory;
+}
