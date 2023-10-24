9328e2e25d787ce69c077a3bee93fea19ef3355a
carrotisorange <thepropertymanager2020@gmail.com>
2023-10-24 12:38:25 +0800

    improved confirm payments

--- resources/views/livewire/collection-tenant-edit-component.blade.php
+++ resources/views/livewire/collection-tenant-edit-component.blade.php
@@ -1,44 +1,52 @@
 <div>
     <div class="md:grid md:grid-cols-1 px-10 md:gap-6">
-        <nav class="mt-5 border-b flex col-start-1" aria-label="Breadcrumb">
-            <ol role="list" class="mx-auto flex w-full max-w-screen-xl space-x-4 px-4 sm:px-6">
+                <nav class="flex border-b border-gray-200 bg-white" aria-label="Breadcrumb">
+                    <ol role="list" class="mx-auto flex w-full max-w-screen-xl space-x-4 px-4 sm:px-6 lg:px-8">
+                        <li class="flex">
+                            <div class="flex items-center">
+                                <a href="/property" class="text-gray-400 hover:text-gray-500">
+                                    <svg class="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                            d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="sr-only">Home</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="flex">
+                            <div class="flex items-center">
+                                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                                </svg>
+                                <a href="/property/{{ Session::get('property_uuid') }}/tenant/{{ $tenant->uuid }}"
+                                    class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">{{ $tenant->tenant }}</a>
+                            </div>
+                        </li>
 
-                <li class="flex">
-                    <div class="flex items-center">
-                        <button onclick="window.location.href='#'"
-                            class="text-lg font-medium text-gray-500 hover:text-gray-700" aria-current="page">
-                            Tenants</button>
-                    </div>
-                </li>
-
-                <li class="flex">
-                    <div class="flex items-center">
-                        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44"
-                            preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true">
-                            <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-                        </svg>
-                        <button onclick="window.location.href='#'"
-                            class="ml-4 text-lg font-medium text-gray-500 hover:text-gray-700 ">
-                            {{ $tenant->tenant }} </button>
-                    </div>
-                </li>
-
-                <li class="flex">
-                    <div class="flex items-center">
-                        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44"
-                            preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true">
-                            <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-                        </svg>
-                        <button type="button" disabled class="ml-4 text-lg font-bold text-gray-700 hover:text-gray-700"
-                            aria-current="page">
-                            Confirm Payments</button>
-                    </div>
-
-                </li>
-            </ol>
-        </nav>
+                        <li class="flex">
+                            <div class="flex items-center">
+                                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                                </svg>
+                                <a href="/property/{{ Session::get('property_uuid') }}/tenant/{{ $tenant->uuid }}/bills"
+                                    class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Bill</a>
+                            </div>
+                        </li>
+                        <li class="flex">
+                            <div class="flex items-center">
+                                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                                </svg>
+                                <a href="#/"
+                                    class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Confirm Payment</a>
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
         <div class="mt-5 md:mt-0 md:col-span-2">
             <div class="grid grid-cols-2 gap-6">
                 <div class="col-span-1">
@@ -260,11 +268,11 @@
     </div>
 
     <div class="flex justify-end p-10 mt-5">
-        <x-button
+        <x-button class="bg-red-500"
             onclick="window.location.href='/property/{{ Session::get('property_uuid') }}/tenant/{{ $tenant->uuid }}/bills'">
             Cancel
         </x-button>
-
+        &nbsp;
         @if($collections->sum('bill') == 0)
 
         @else
