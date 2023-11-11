85d494a38d761340e4bffbb96be0697bdca5aa44
Aira Cortez <arpct1441@gmail.com>
2023-11-11 14:47:08 +0800

    fixed broken links

--- routes/web.php
+++ routes/web.php
@@ -110,11 +110,30 @@ Route::get('/profile-setup', function(){
 });
 
 Route::get('/propsuite', function(){
-    return view('propsuite.propsuite');
+    return view('landing.propsuite.propsuite');
 });
 
-Route::get('/propsuite2', function(){
-    return view('propsuite.propsuite2');
+Route::get('/propsuite-lite', function(){
+    return view('landing.propsuite.propsuite-lite');
+});
+
+Route::get('/propsuite-daily', function(){
+    return view('landing.propsuite.propsuite-daily');
+});
+
+Route::get('/propsuite-hoa', function(){
+    return view('landing.propsuite.propsuite-hoa');
+});
+
+Route::get('/propsuite-condo', function(){
+    return view('landing.propsuite.propsuite-condo');
+});
+Route::get('/resources', function(){
+    return view('landing.resources');
+});
+
+Route::get('/pricing', function(){
+    return view('landing.pricing');
 });
 
 Route::get('/proprent', function(){
