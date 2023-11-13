5b79b8f41aec180804410dab19c2371a83b18d1b
carrotisorange <thepropertymanager2020@gmail.com>
2023-11-13 09:38:07 +0800

    fixed issue with rfp routes

--- routes/features/rfp.php
+++ routes/features/rfp.php
@@ -17,18 +17,18 @@ use Illuminate\Support\Facades\Route;
     });
 
     Route::controller(RFPController::class)->group(function () {
-        Route::prefix('{accountPayable}')->group(function(){
-        Route::get('/','show')->name('rfp');
-        Route::get('{batch_no}/store', 'store');
-        Route::get('step1/export', 'download_step_1');
-        Route::get('step-1', 'create_step_1')->name('rfp');
-        Route::get('step-2', 'create_step_2')->name('rfp');
-        Route::get('step-3', 'create_step_3')->name('rfp');
-        Route::get('step-4', 'create_step_4')->name('rfp');
-        Route::get('step-5', 'create_step_5')->name('rfp');
-        Route::get('step-6', 'create_step_6')->name('rfp');
-        Route::get('step-7', 'create_step_7')->name('rfp');
-        Route::get('download', 'download');
+        Route::prefix('{accountpayable}')->group(function(){
+            Route::get('/','show')->name('rfp');
+            Route::get('{batch_no}/store', 'store');
+            Route::get('step1/export', 'download_step_1');
+            Route::get('step-1', 'create_step_1')->name('rfp');
+            Route::get('step-2', 'create_step_2')->name('rfp');
+            Route::get('step-3', 'create_step_3')->name('rfp');
+            Route::get('step-4', 'create_step_4')->name('rfp');
+            Route::get('step-5', 'create_step_5')->name('rfp');
+            Route::get('step-6', 'create_step_6')->name('rfp');
+            Route::get('step-7', 'create_step_7')->name('rfp');
+            Route::get('download', 'download');
         });
 
         Route::get('/', 'index')->name('rfp');
