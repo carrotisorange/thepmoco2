0777c6a485618da8eb19d1138161d7b3bbd60858
carrotisorange <thepropertymanager2020@gmail.com>
2023-11-21 11:46:31 +0800

    Update sendbilltotenant.blade.php

--- resources/views/emails/sendbilltotenant.blade.php
+++ resources/views/emails/sendbilltotenant.blade.php
@@ -1,21 +1,25 @@
 @component('mail::message')
 Hi, {{ $data['tenant'] }}!
+
 Date: {{ Carbon\Carbon::now()->format('M d, Y') }}
+
 Due Date: {{ Carbon\Carbon::parse($data['due_date'])->format('M d, Y') }}
+
 Bills to be Paid: {{ number_format($data['balance'], 2) }}
+
 Bills to be Paid After Due Date: {{ number_format($data['balance_after_due_date'], 2) }}
 
 Bills Breakdown
 
 @component('mail::table')
-    | Bill # | Date Posted | Unit | Particular | Period Covered | Amount |
-    | ------------- |------------- |------------- |------------- |-------|
-    @foreach ($data['bills'] as $item)
-    | {{ $item->bill_no }} | {{ Carbon\Carbon::parse($item->created_at)->format('M d, Y') }} | {{ $item->unit->unit }} |  {{ $item->particular->particular }} | {{Carbon\Carbon::parse($item->start)->format('M d, Y').'-'.Carbon\Carbon::parse($item->end)->format('M d, Y') }} | {{number_format((($item->bill)-App\Models\Collection::where('bill_id', $item->id)->sum('collection')),2) }}|
-    @endforeach
-    | ------------- |------------- |------------- |------------- |-------|
-    |  Total        |              |              |              |{{ number_format($data['balance'], 2) }}|
+| Bill # | Date Posted | Unit | Particular | Period Covered | Amount |
+| ------------- |------------- |------------- |------------- |------------- |------------- |
+@foreach ($data['bills'] as $item)
+| {{ $item->bill_no }} | {{ Carbon\Carbon::parse($item->created_at)->format('M d, Y') }} | {{ $item->unit->unit }} |{{ $item->particular->particular }} | {{Carbon\Carbon::parse($item->start)->format('M d, Y').'-'.Carbon\Carbon::parse($item->end)->format('M d, Y') }} |{{number_format((($item->bill)-App\Models\Collection::where('bill_id', $item->id)->sum('collection')),2) }}|
+@endforeach
+
 @endcomponent
+
 @if($data['note_to_bill'])
 {{ $data['note_to_bill'] }}
 @endif
@@ -23,7 +27,9 @@ Bills Breakdown
 
 Regards,<br>
 {{ Session::get('property') }}
-<br>
+<br><br>
 For inquiries reach us at: {{ Session::get('property_email') }} / {{ Session::get('property_mobile') }}
+<br><br>
 
 @endcomponent
+
