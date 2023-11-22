ff39bf8b7a1943b01ee6546dfa599eaeece56880
Aira Cortez <arpct1441@gmail.com>
2023-11-22 12:25:56 +0800

    maintenance page

---
+++ resources/views/properties/server-maintenance.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2.2.7/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="canonical" href="http://propsuite.net" />
+    <title>System Advisory</title>
+
+
+<style>
+    body {
+        font-family: 'Poppins';
+    }
+</style>
+
+<html>
+
+    <body class="py-5 px-10 lg:py-32 md:px-32 lg:px-56 mx-auto">
+        <img class="h-14 m-3" src="{{ asset('/brands/propsuite/propsuite-gray.png') }}">
+        <div class="block lg:flex justify-between">
+            <div class="max-w-lg">
+                <h1 class="ml-5 font-bold text-3xl text-gray-800">System Advisory</h1>
+                <p class="mt-5 ml-5 text-base font-light text-gray-600">
+                    PropSuite will undergo a <span class="font-semibold">system maintenance activity</span> from <span class="font-semibold">Date and Time of Activity.</span>
+                </p>
+                <p class="mt-5 ml-5 text-base font-light text-gray-600">
+                    During this activity, PropSuite services will be unavailable.
+                </p>
+                <p class="ml-5 text-base font-light text-gray-600">
+                    This is part of our regular maintenance to ensure PropSuite system's continued stable performance.
+                </p>
+                <p class="mt-5 ml-5 text-base font-light text-gray-600">
+                    We apologize for any inconvenience this may cause.
+                </p>
+                <p class="ml-5 text-base font-light text-gray-600">
+                    We advise you to do your transactions ahead of the scheduled activity. Thank you.
+                </p>
+            
+            </div>
+            <div class="block lg:flex justify-start">
+            <img class="w-full" src="{{ asset('/brands/server-maintenance.png') }}">
+            </div>
+        </div>
+    </body>
+
+</html>
