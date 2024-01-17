927a4ef6ce08f8ece883816e6bf9f2ad2c74339a
Aira Cortez <arpct1441@gmail.com>
2024-01-17 11:20:38 +0800

    improved text alignment

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -5,7 +5,7 @@
 ->orderBy('feature_id')
 ->get(); ?>
 <nav aria-label="Sidebar" class="hidden md:block md:flex-shrink-0 md:bg-white overflow-auto h-screen pb-32 text-center">
-    <div class="relative flex w-22 flex-col space-y-3 p-3">
+    <div class="relative flex w-22 flex-col space-y-3 p-3 text-center items-center">
         @foreach($availableFeatures as $feature)
             @if($feature->feature->is_active)
                 @if(Session::get('property_uuid'))
@@ -20,7 +20,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
                 </x-nav-link>
 
-                <p class="font-medium leading-3 ml-0 text-xs text-gray-900 mt-10">
+                <p class="font-medium text-xs text-gray-900 mt-10 text-center">
                     {{ $feature->feature->feature }}
                 </p>
             @endif
